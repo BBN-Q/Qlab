c216e41ca0469f8dfcd01e92b174dfdb8eac1b64
bjohnson <bjohnson@005577ff-ac90-44ea-9eee-c3a23b7cba77>
2011-03-01 16:05:06 +0000

    Adding driver for HP71000 spectrum analyzer. Warning: this driver has not yet been tested. Committing so that I can test on a machine with a GPIB card.
    git-svn-id: https://qubit.bbn.com/svn/MQCO/trunk@43 005577ff-ac90-44ea-9eee-c3a23b7cba77

---
+++ common/src/+deviceDrivers/@HP71000/HP71000.m
@@ -0,0 +1,150 @@
+% Copyright 2010 Raytheon BBN Technologies
+%
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+%
+%     http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+%
+% File: HP71000.m
+% Author: Blake Johnson (bjohnson@bbn.com)
+%
+% Description: Instrument driver for the HP71000 spectrum analyzer.
+
+classdef HP71000 < deviceDrivers.lib.GPIB
+   properties
+       center_frequency
+       span
+       resolution_bw
+       video_bw
+   end
+   
+   methods
+       function obj = HP71000()
+           
+       end
+       
+       function sweep(obj)
+           obj.Write('TS;');
+       end
+       
+       function val = peakAmplitude(obj)
+           obj.sweep(); % take a sweep
+           obj.Write('MKPK HI;'); % move marker to peak
+           tmp = obj.Query('MKA?;'); % get marker amplitude
+           val = str2double(tmp);
+       end
+       
+       function val = peakFrequency(obj)
+           obj.sweep(); % take a sweep
+           obj.Write('MKPK HI;'); % move marker to peak
+           tmp = obj.Query('MKF?;'); % get marker frequency
+           val = str2double(tmp);
+       end
+       
+       function val = downloadTrace(obj)
+           obj.sweep(); % take a sweep
+           tmp = obj.Query('TRA?;');
+           val = str2double(tmp);
+       end
+       
+       % instrument meta-setter
+		function setAll(obj, settings)
+			fields = fieldnames(settings);
+			for j = 1:length(fields);
+				name = fields{j};
+				if ismember(name, methods(obj))
+					feval(['obj.' name], settings.(name));
+				elseif ismember(name, properties(obj))
+					obj.(name) = settings.(name);
+				end
+			end
+        end
+        
+        % property accessors
+        
+        function val = get.center_frequency(obj)
+            temp = obj.Query('CF?;');
+            val = str2double(temp);
+        end
+        
+        function val = get.span(obj)
+            temp = obj.Query('SP?;');
+            val = str2double(temp);
+        end
+        
+        function val = get.resolution_bw(obj)
+            temp = obj.Query('RB?;');
+            val = str2double(temp);
+        end
+        
+        function val = get.video_bw(obj)
+            temp = obj.Query('VB?;');
+            val = str2double(temp);
+        end
+        
+        % property settors
+        
+        function set.center_frequency(obj, value)
+            gpib_string = 'CF %dGHz;';
+
+            % Validate input
+            if ~isnumeric(value)
+                error('Invalid input');
+            end
+            
+            gpib_string = sprintf(gpib_string, value/1e9);
+
+            obj.Write(gpib_string);
+        end
+        
+        function set.span(obj, value)
+            gpib_string = 'SP %dMHz;';
+
+            % Validate input
+            if ~isnumeric(value)
+                error('Invalid input');
+            end
+            
+            gpib_string = sprintf(gpib_string, value/1e6);
+
+            obj.Write(gpib_string);
+        end
+        
+        function set.resolution_bw(obj, value)
+            gpib_string = 'RB %dkHz;';
+
+            % Validate input
+            if strcmp(value, 'auto')
+                gpib_string = 'RB AUTO;';
+            elseif isnumeric(value)
+                gpib_string = sprintf(gpib_string, value/1e3);
+            else
+                error('Invalid input');
+            end
+            
+            obj.Write(gpib_string);
+        end
+        
+        function set.video_bw(obj, value)
+            gpib_string = 'VB %dkHz;';
+
+            % Validate input
+            if strcmp(value, 'auto')
+                gpib_string = 'VB AUTO;';
+            elseif isnumeric(value)
+                gpib_string = sprintf(gpib_string, value/1e3);
+            else
+                error('Invalid input');
+            end
+
+            obj.Write(gpib_string);
+        end
+   end
+end
--- common/src/+deviceDrivers/@Yoko7651/Yoko7651.m
+++ common/src/+deviceDrivers/@Yoko7651/Yoko7651.m
@@ -1,9 +1,24 @@
+% Copyright 2010 Raytheon BBN Technologies
+%
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+%
+%     http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+%
+% File: Yoko7651.m
+% Author: Blake Johnson (bjohnson@bbn.com)
+% Generated on: Tues Oct 19 2010
+%
+% Description: Instrument driver for the Yokogawa 7651 DC source
+
 classdef (Sealed) Yoko7651 < deviceDrivers.lib.GPIB
-    % Yokogawa 7651 DC source
-    %
-    %
-    % Author(s): Blake Johnson
-    % Generated on: Tues Oct 19 2010
     
     % Class-specific constant properties
     properties (Constant = true)
