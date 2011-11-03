c617d838f7c75b714bef98618a0ae1fa0d8d9791
bdonovan <bdonovan@005577ff-ac90-44ea-9eee-c3a23b7cba77>
2011-11-03 21:06:22 +0000

    Added copy standalone driver script
    git-svn-id: https://qubit.bbn.com/svn/MQCO/trunk@194 005577ff-ac90-44ea-9eee-c3a23b7cba77

--- hardware/APS/matlab/chkout_stdalone_driver.sh
+++ hardware/APS/matlab/chkout_stdalone_driver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-repository=https://qisrep.bbn.com/svn/qlab/trunk
+repository=https://qubit.bbn.com/svn/MQCO/trunk
 waveform=$repository/common/src/util/
 driver=$repository/common/src/+deviceDrivers/@DacII@
 
@@ -20,17 +20,16 @@ cp util/DacIIGui/* ./dacii/
 
 rm -rf util
 rm -rf \@DacII
-rm -rf DacIIGui
 
 # remove reference to experiment framework from DacII gui
-sed -i 's/classdef DacII < deviceDrivers.lib.deviceDriverBase/classdef dacII < handle/g' ./dacii/dacII.m
+sed -i 's/classdef DacII < deviceDrivers.lib.deviceDriverBase/classdef DacII < handle/g' ./dacii/dacII.m
 sed -i 's/d = d@deviceDrivers.lib.deviceDriverBase/%d = d@deviceDrivers.lib.deviceDriverBase/g' ./dacii/dacII.m
 # convert line endings back to windows
 sed -i 's/$'"/`echo \\\r`/"  ./dacii/dacII.m       
 
 
 # rebuild libdacii.dll
-cd dacii\lib
+cd dacii/lib
 make clean
 make all
 cd -
