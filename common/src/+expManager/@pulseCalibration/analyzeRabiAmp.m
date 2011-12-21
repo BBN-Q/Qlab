7192701328c45ad5530b2cf80792ce4be37a2080
bjohnson <bjohnson@005577ff-ac90-44ea-9eee-c3a23b7cba77>
2011-12-21 23:42:05 +0000

    Incremental progress on updating analyzeRabiAmp().
    git-svn-id: https://qubit.bbn.com/svn/MQCO/trunk@268 005577ff-ac90-44ea-9eee-c3a23b7cba77

--- common/src/+expManager/@pulseCalibration/analyzeRabiAmp.m
+++ common/src/+expManager/@pulseCalibration/analyzeRabiAmp.m
@@ -1,4 +1,13 @@
 function amp = analyzeRabiAmp(data)
+    xpts = 0:200:40*200;
+    % use largest FFT frequency component to seed Rabi frequency
+    yfft = fft(data);
+    [tmp freqpos] = max(abs( yfft(2:floor(end/2)) ));
+    frabi = 2*pi*(freqpos-1)/xpts(end);
+    
+    % model A + B * cos(w t + phi)
+    rabif = inline('p(1) + p(2)*cos(p(3)*t + p(4))','p','t');
 
-
+    % initial guess for amplitude is max - min
+    amp = max(data) - min(data);
 end
