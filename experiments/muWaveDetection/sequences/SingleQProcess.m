14e467ae14cb841561990a0cf4abb3dd05e78319
CoherenceController <qlab@bbn.com>
2012-02-10 15:06:40 -0500

    Adding single-qubit process tomography.

---
+++ experiments/muWaveDetection/sequences/SingleQProcess.m
@@ -0,0 +1,57 @@
+function SingleQProcess(varargin)
+
+%varargin assumes qubit and then makePlot
+qubit = 'q1';
+makePlot = true;
+
+if length(varargin) == 1
+    qubit = varargin{1};
+elseif length(varargin) == 2
+    qubit = varargin{1};
+    makePlot = varargin{2};
+elseif length(varargin) > 2
+    error('Too many input arguments.')
+end
+
+basename = 'SingleQProcess';
+fixedPt = 3000;
+cycleLength = 7000;
+nbrRepeats = 2;
+
+% load config parameters from file
+params = jsonlab.loadjson(getpref('qlab', 'pulseParamsBundleFile'));
+qParams = params.(qubit);
+qubitMap = jsonlab.loadjson(getpref('qlab','Qubit2ChannelMap'));
+IQkey = qubitMap.(qubit).IQkey;
+
+% if using SSB, uncomment the following line
+% params.(IQkey).T = eye(2);
+pg = PatternGen('dPiAmp', qParams.piAmp, 'dPiOn2Amp', qParams.pi2Amp, 'dSigma', qParams.sigma, 'dPulseType', qParams.pulseType, 'dDelta', qParams.delta, 'correctionT', params.(IQkey).T, 'dBuffer', qParams.buffer, 'dPulseLength', qParams.pulseLength, 'cycleLength', cycleLength, 'linkList', params.(IQkey).linkListMode);
+patseq = {};
+
+pulses = {'QId', 'Xp', 'X90p', 'Y90p', 'X90m', 'Y90m'};
+pulseLib = struct();
+for i = 1:length(pulses)
+    pulseLib.(pulses{i}) = pg.pulse(pulses{i});
+end
+
+process = pulseLib.X90p;
+
+for ii = 1:6
+    for jj = 1:6
+            patseq{end+1} = { pulseLib.(pulses{ii}), process, pulseLib.(pulses{jj}) };
+    end
+end
+                
+
+calseq = {};
+calseq{end+1} = {pg.pulse('QId')};
+calseq{end+1} = {pg.pulse('Xp')};
+
+compiler = ['compileSequence' IQkey];
+compileArgs = {basename, pg, patseq, calseq, 1, nbrRepeats, fixedPt, cycleLength, makePlot, 10};
+if exist(compiler, 'file') == 2 % check that the pulse compiler is on the path
+    feval(compiler, compileArgs{:});
+end
+
+end
