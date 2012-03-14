16089e6490cd56ee25c9fa28f6d55d43de092a2f
CoherenceController <qlab@bbn.com>
2012-03-14 15:21:32 -0400

    Got the APS compiler exporting the new HDF5 layout. --CAR

--- common/src/util/exportAPSConfig.m
+++ common/src/util/exportAPSConfig.m
@@ -1,48 +1,92 @@
-function exportAPSConfig(path, basename, ch12seq, ch34seq)
-    Version = 1.5;
+function exportAPSConfig(path, basename, varargin)
+    %varargin is the IQ pairs of channels e.g. ch56seq
+    %pass an empty array to skip a pair of channels
+    VersionNum = 1.6;
+    
     useVarients = 1;
     
     % construct filename
     disp('Writing APS file');
-    fullpath = strcat(path, basename, '.mat');
+    fileName = strcat(path, basename, '.h5');
     if ~exist(path, 'dir')
         mkdir(path);
     end
     
     miniLinkRepeat = 0;
-    WaveformLibs = {};
-    LinkLists = {};
-    bankStruct = struct('offset', [], 'count', [], 'trigger', [], 'repeat', [], 'length', 0);
+    WaveformLibs = cell(1,4);
+    LinkLists = cell(1,4);
     
-    for ii = [1,3]
-        % convert link lists to APS format
-        varname = ['ch' num2str(ii) num2str(ii+1) 'seq'];
-        if exist(varname, 'var')
-            % set up structs
-            LinkLists{ii} = struct('bankA', bankStruct, 'bankB', bankStruct, 'repeatCount', 0);
-            LinkLists{ii+1} = struct('bankA', bankStruct, 'bankB', bankStruct, 'repeatCount', 0);
-            seq = eval(varname);
+    for ct = 1:length(varargin)
+        Ich = 2*ct-1;
+        Qch = 2*ct;
+        if ~isempty(varargin{ct})
+            seq = varargin{ct};
             [xWfLib, yWfLib] = APSPattern.buildWaveformLibrary(seq, useVarients);
             [wf, xbanks] = APSPattern.convertLinkListFormat(seq, useVarients, xWfLib, miniLinkRepeat);
-            WaveformLibs{ii} = wf.prep_vector();
+            WaveformLibs{Ich} = wf.prep_vector();
             [wf, ybanks] = APSPattern.convertLinkListFormat(seq, useVarients, yWfLib, miniLinkRepeat);
-            WaveformLibs{ii+1} = wf.prep_vector();
-            LinkLists{ii}.bankA = xbanks{1};
-            LinkLists{ii+1}.bankA = ybanks{1};
-            fprintf('Length of Bank A: %d\n',LinkLists{ii}.bankA.length)
-            if length(xbanks) > 1
-                LinkLists{ii}.bankB = xbanks{2}; 
-                LinkLists{ii+1}.bankB = ybanks{2}; 
-                fprintf('Length of Bank B: %d\n',LinkLists{ii}.bankB.length)
-            end
+            WaveformLibs{Qch} = wf.prep_vector();
+
+            %Setup the structures for the linkList data
+            LinkLists{Ich} = struct('repeatCount', 0);
+            LinkLists{Qch} = struct('repeatCount', 0);
+            LinkLists{Ich}.numBanks = length(xbanks);
+            for bankct = 1:LinkLists{Ich}.numBanks
+                bankStr = sprintf('bank%d',bankct);
+                LinkLists{Ich}.(bankStr) = xbanks{bankct};
+                fprintf('Length of Bank %d: %d\n', bankct, LinkLists{Ich}.(bankStr).length)
+            end 
+            LinkLists{Qch}.numBanks = length(ybanks);
+            for bankct = 1:LinkLists{Qch}.numBanks
+                LinkLists{Qch}.(sprintf('bank%d',bankct)) = ybanks{bankct};
+            end 
         else
-            WaveformLibs{ii} = [];
-            LinkLists{ii} = [];
-            WaveformLibs{ii+1} = [];
-            LinkLists{ii+1} = [];
+            WaveformLibs{Ich} = [];
+            LinkLists{Ich} = [];
+            WaveformLibs{Qch} = [];
+            LinkLists{Ich} = [];
         end
     end
     
-    save(fullpath, 'Version', 'WaveformLibs', 'LinkLists', '-v7.3');
-    
+    %Figure out which channels have data
+    channelDataFor = uint16(find(cellfun(@(x) ~isempty(x), WaveformLibs)));
+
+    %Now write things out to the hdf5 file
+    if exist(fileName, 'file')
+        delete(fileName);
+    end
+    %First which channel we have data for
+    h5create(fileName, '/channelDataFor', size(channelDataFor), 'Datatype', 'uint16');
+    h5write(fileName, '/channelDataFor', channelDataFor);
+    h5writeatt(fileName,'/', 'Version Number', VersionNum);
+    %Now create each channel group and put the associated data
+    for channel = channelDataFor
+        channelStr = sprintf('/channel%d', channel);
+        
+        %The waveform library
+        h5create(fileName, [channelStr, '/waveformLib'], size(WaveformLibs{channel}), 'Datatype', 'int16');
+        h5write(fileName, [channelStr, '/waveformLib'], WaveformLibs{channel});
+        
+        %The linklist data
+        %First the number of banks
+        h5create(fileName, [channelStr, '/linkListData/numBanks'], [1, 1], 'Datatype', 'uint16');
+        h5write(fileName, [channelStr, '/linkListData/numBanks'], uint16(LinkLists{channel}.numBanks));
+        %Now loop over each bank
+        for bankct = 1:LinkLists{channel}.numBanks
+            bankStr = sprintf('bank%d',bankct);
+            curBank = LinkLists{channel}.(bankStr);
+            groupStr = [channelStr, '/linkListData/', bankStr];
+            bankLength = double(curBank.length);
+            h5create(fileName, [groupStr, '/length'], [1, 1], 'Datatype', 'uint16');
+            h5write(fileName, [groupStr, '/length'], uint16(bankLength));
+            h5create(fileName, [groupStr, '/offset'], [1, bankLength], 'Datatype', 'uint16');
+            h5write(fileName, [groupStr, '/offset'], curBank.offset);
+            h5create(fileName, [groupStr, '/count'], [1, bankLength], 'Datatype', 'uint16');
+            h5write(fileName, [groupStr, '/count'], curBank.count);
+            h5create(fileName, [groupStr, '/trigger'], [1, bankLength], 'Datatype', 'uint16');
+            h5write(fileName, [groupStr, '/trigger'], curBank.trigger);
+            h5create(fileName, [groupStr, '/repeat'], [1, bankLength], 'Datatype', 'uint16');
+            h5write(fileName, [groupStr, '/repeat'], curBank.repeat);
+        end
+    end
 end
