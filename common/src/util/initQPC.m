e5ea5c0648ef7c754caa7e884c568783608031b5
taohki <tohki@bbn.com>
2012-05-03 11:01:26 -0400

    Made marker channel input to initQPC an array for consistency and updated help string. --CAR

--- common/src/util/initQPC.m
+++ common/src/util/initQPC.m
@@ -1,13 +1,13 @@
 % QPC initialization
 %
-% Author(s): TAO
+% Authors: TAO, CAR
 % Date created: April 26 2012
-% function initQPC(mrknum1, mrknum2, mrknum3,...
-%                   sernum1,sernum2,sernum3,...
-%                       fr1,fr2,fr3)...
-% mrknum# is APS marker number connected to Switch TRIG   eg. 1,2,3
-% sernum# is LabBrick serial for associated Switch TRIG   eg. 1692
-% fr# is the LabBrick frequency(GHZ)used for each sernum# eg. 5.1
+% function initQPC(mrkNums, serialNums, freqs)
+%
+% mrknums is an array APS marker channels connected to Switch TRIG   eg. [1, 3]
+% serialNums is an array of LabBrick serial numbers for associated Switch
+% TRIG eg. [1692, 1685] 
+% freqs is an array the LabBrick frequencies to be used eg. [5.1, 6.2]
 
 % Copyright 2012 Raytheon BBN Technologies
 %
@@ -22,11 +22,17 @@
 % WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 % See the License for the specific language governing permissions and
 % limitations under the License.
-function initQPC(mrknum1, mrknum2, mrknum3,serialNums,freqs)
+function initQPC(mrkNums, serialNums, freqs)
 
 %Clean up any old instruments (not particularly friendly to other
-%instruments)
+%programs)
 delete(instrfind);
+
+assert(length(mrkNums) == length(serialNums), 'Oops! We need the same number of marker channels as LabBrick serial numbers.');
+assert(length(serialNums) == length(freqs), 'Oops!, We need the same number of frequencies as LabBrick serial numbers.');
+
+numChannels = length(mrkNums);
+
 
 %Setup some basic settings for the APS with all the channels off 
 fprintf('Initializing APS\n');
@@ -45,20 +51,21 @@ settings.chan_4.amplitude = 1.0;
 settings.chan_4.offset = 0;
 settings.samplingRate = 1200;
 settings.triggerSource = 'internal';
-settings.seqfile = 'C:\AWG\initQPC\initQPCBBNAPS12.mat';
+settings.seqfile = 'U:\APS\initQPC\initQPCBBNAPS12.mat';
 settings.seqforce = true;
 
 
 %Open and connect to spectrum analyzer (must modify this for individual setups eg address,cmds etc.)
 %BBN is currently using a HP71000 SA GPIB 18
-% speca= deviceDrivers.HP71000();
-% speca.connect(18);
-speca= deviceDrivers.AnritsuMS271xB();
-speca.connect('9.2.178.135');
+speca= deviceDrivers.HP71000();
+speca.connect(18);
+% speca= deviceDrivers.AnritsuMS271xB();
+% speca.connect('9.2.178.135');
 
 %Open Labricks
-labBricks(3,1) = deviceDrivers.Labbrick();
-for ct = 1:3
+
+labBricks(numChannels,1) = deviceDrivers.Labbrick();
+for ct = 1:numChannels
     try
         labBricks(ct).connect(serialNums(ct))
     catch exception
@@ -68,7 +75,6 @@ for ct = 1:3
 end
 
 %open AWG
-chan=[mrknum1 mrknum2 mrknum3];
 awg = deviceDrivers.APS();
 awg.open(0,1);
 if ~awg.is_open
@@ -78,13 +84,13 @@ awg.stop()
 forceLoadBitFile = 0;
 awg.init(forceLoadBitFile);
 awg.setAll(settings);
-ch_fields = {['chan_' num2str(mrknum1)], ['chan_' num2str(mrknum2)], ['chan_' num2str(mrknum3)]};
+ch_fields = arrayfun(@(x) ['chan_' int2str(x)], mrkNums, 'UniformOutput', false);
 a = zeros(2,1);
-for ct = 1:length(ch_fields)
+for ct = 1:numChannels
     ch = ch_fields{ct};
-    awg.setLinkListMode(chan(ct)-1, awg.LL_ENABLE, awg.LL_ONESHOT);
+    awg.setLinkListMode(mrkNums(ct)-1, awg.LL_ENABLE, awg.LL_ONESHOT);
     awg.(ch).enabled= true;
-    disp(['measuring_chan_' num2str(chan(ct))])
+    disp(['Measuring chan_' num2str(mrkNums(ct))])
     %Now flip the switch 
     for j=1:2
         speca.center_frequency = freqs(ct)*1e9;
@@ -99,18 +105,18 @@ for ct = 1:length(ch_fields)
     end
     %Check to see which direction had the switch open or closed
     if a(2)<a(1)
-        display('inverted')
+        display('Channel was inverted')
         awg.run();
         awg.stop();
-        disp(['isolation ' num2str(a(1)-a(2)) 'dB'])
+        disp(['Isolation: ' num2str(a(1)-a(2)) 'dB'])
     else 
-        disp(['isolation ' num2str(a(2)-a(1)) 'dB'])
+        disp(['Isolation ' num2str(a(2)-a(1)) 'dB'])
     end
     awg.(ch).enabled = false;
-    disp([ch '_initialized'])
+    disp([ch ' initialized'])
 end
 disp('QPC initialization complete')
-for ct = 1:3
+for ct = 1:numChannels
     labBricks(ct).disconnect();
 end
 awg.close();
