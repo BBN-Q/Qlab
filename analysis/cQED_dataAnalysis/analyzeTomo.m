4020706fc464c91c87666d7486936d07994fc32f
Coherence Controller <qlab@bbn.com>
2012-05-31 13:18:17 -0400

    Generalize 2 qubit state tomography for variable nbrRepeats.
    analyzeTomo now takes in the nbrRepeats and all the subfunctions deal with single-point averaged data.
    --CAR and BRJ

--- analysis/cQED_dataAnalysis/analyzeTomo.m
+++ analysis/cQED_dataAnalysis/analyzeTomo.m
@@ -1,5 +1,12 @@
-function [beta, paulis, C] = analyzeTomo(data)
-    [beta paulis] = PauliTomo(data);
+function [beta, paulis, C] = analyzeTomo(data, nbrRepeats)
+
+    %Analyze two qubit state tomography
+    %Expects calibration data then tomography data
+    
+    calData = mean(reshape(data(1:4*nbrRepeats), nbrRepeats, 4), 1);
+    tomoData = mean(reshape(data(4*nbrRepeats+1:end), nbrRepeats, 16), 1);
+
+    [beta paulis] = PauliTomo(calData, tomoData);
     figure;
     bar(paulis)
     PauliLabel();
