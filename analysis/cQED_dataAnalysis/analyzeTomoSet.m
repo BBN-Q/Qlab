8b2231bc4d9b8dece077a73caa1a9c323f6f5585
Coherence Controller <qlab@bbn.com>
2012-10-09 17:03:10 -0400

    Few bits and bobs to improve the tomography code.
    --CAR and BRJ

--- analysis/cQED_dataAnalysis/analyzeTomoSet.m
+++ analysis/cQED_dataAnalysis/analyzeTomoSet.m
@@ -1,17 +1,38 @@
 function concurrence = analyzeTomoSet(data)
 
 concurrence = zeros(size(data.abs_Data,1),1);
+
+pauliLabels = {'II', 'XI', 'YI', 'ZI', 'IX', 'IY', 'IZ', 'XY', 'XZ', 'YX', 'YZ', 'ZX', 'ZY', 'XX', 'YY', 'ZZ'};
+nbrRepeats = 2;
+nbrQubits = 2;
+[pauliOps, pauliStrs] = paulis(nbrQubits);
+
+paulisWiz = zeros(length(concurrence),16);
 for ct = 1:length(concurrence)
     % extract cals and data
-    nbrRepeats = 2;
-    nbrQubits = 2;
     thisSlice = data.abs_Data(ct, :);
-    calData = mean(reshape(thisSlice(1:nbrRepeats*2^nbrQubits), nbrRepeats, 2^nbrQubits), 1);
-    tomoData = mean(reshape(thisSlice(nbrRepeats*2^nbrQubits+1:end), nbrRepeats, 4^nbrQubits), 1);
+    tomoData = mean(reshape(thisSlice(1:end-nbrRepeats*2^nbrQubits), nbrRepeats, 4^nbrQubits), 1);
+    calData = mean(reshape(thisSlice(end-nbrRepeats*2^nbrQubits+1:end), nbrRepeats, 2^nbrQubits), 1);
 
     [~, raw_paulis] = PauliTomo(calData, tomoData);
     rhoRaw = getRho(raw_paulis);
     rhoWizard = WizardTomo_(rhoRaw, nbrQubits);
+    for paulict = 1:length(pauliOps)
+        pauliNum = find(strcmp(pauliLabels{paulict}, pauliStrs));
+        paulisWiz(ct,paulict) = real(trace(rhoWizard*pauliOps{pauliNum}));
+    end
     fprintf('Purity ct %d: %f\n', ct, trace(rhoWizard^2));
     concurrence(ct) = Concurrence_(rhoWizard);
 end
+
+figure()
+plot(0:pi/64:pi, paulisWiz + repmat(0:2:30, 65, 1));
+set(gca(), 'YTick', 0:2:30);
+set(gca(), 'YTickLabel', pauliLabels);
+ylim([-1, 31]);
+xlim([0,pi]);
+set(gca(), 'YGrid', 'On');
+xlabel('CR Gate Angle (rad.)');
+
+
+
