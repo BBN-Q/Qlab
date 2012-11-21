bd603469eb0e2690ec12924f22f95c4f87c32281
CoherenceController@Draco <qlab@bbn.com>
2012-11-20 22:19:28 -0500

    Update single-shot sequence for new PatternGen.
    --BRJ

--- common/src/+expManager/@singleShotFidelity/SingleShotSequence.m
+++ common/src/+expManager/@singleShotFidelity/SingleShotSequence.m
@@ -2,19 +2,12 @@ function SingleShotSequence(qubit)
 
 basename = 'SingleShot';
 fixedPt = 2000;
-cycleLength = 5000;
+cycleLength = 7100;
 nbrRepeats = 1;
-
-% load config parameters from files
-params = jsonlab.loadjson(getpref('qlab', 'pulseParamsBundleFile'));
-qParams = params.(qubit);
-qubitMap = jsonlab.loadjson(getpref('qlab','Qubit2ChannelMap'));
-IQkey = qubitMap.(qubit).IQkey;
 
 % if using SSB, set the frequency here
 SSBFreq = -100e6;
-
-pg = PatternGen('dPiAmp', qParams.piAmp, 'dPiOn2Amp', qParams.pi2Amp, 'dSigma', qParams.sigma, 'dPulseType', qParams.pulseType, 'dDelta', qParams.delta, 'correctionT', params.(IQkey).T, 'dBuffer', qParams.buffer, 'dPulseLength', qParams.pulseLength, 'cycleLength', cycleLength, 'linkList', params.(IQkey).linkListMode, 'dmodFrequency',SSBFreq);
+pg = PatternGen(qubit, 'SSBFreq', SSBFreq, 'cycleLength', cycleLength);
 
 patseq = {{pg.pulse('QId')}, {pg.pulse('Xp')}};
 calseq  = [];
@@ -27,14 +20,18 @@ seqParams = struct(...
     'nbrRepeats', nbrRepeats, ...
     'fixedPt', fixedPt, ...
     'cycleLength', cycleLength, ...
-    'measLength', 2000);
+    'measLength', 5000);
 patternDict = containers.Map();
 if ~isempty(calseq), calseq = {calseq}; end
+
+qubitMap = jsonlab.loadjson(getpref('qlab','Qubit2ChannelMap'));
+IQkey = qubitMap.(qubit).IQkey;
+
 patternDict(IQkey) = struct('pg', pg, 'patseq', {patseq}, 'calseq', calseq, 'channelMap', qubitMap.(qubit));
 measChannels = {'M1'};
-awgs = {'TekAWG', 'BBNAPS1', 'BBNAPS2'};
+awgs = {'TekAWG'};
 
-compileSequences(seqParams, patternDict, measChannels, awgs, false, 0);
+compileSequences(seqParams, patternDict, measChannels, awgs, false);
 
 
 end
