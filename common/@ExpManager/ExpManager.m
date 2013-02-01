dd951b1d6b62e01ad4c36f33798b03c2ca404312
Qlab <qlab@bbn.com>
2013-02-01 11:42:04 -0500

    Swept data plotting.
    Also store data in ExpManager object.
    
    --CAR and BRJ

--- common/@ExpManager/ExpManager.m
+++ common/@ExpManager/ExpManager.m
@@ -5,17 +5,16 @@ classdef ExpManager < handle
         instrSettings = struct();
         measurements = struct();
         sweeps = {}
-        name
+        data = struct();
         scopes
         AWGs
         listeners = {}
-        plotTimer
+        plotScopeTimer
     end
     
     methods
         %Constructor
-        function obj = expManager(name)
-            obj.name = name;
+        function obj = ExpManager()
         end
         
         %Destructor
@@ -27,10 +26,8 @@ classdef ExpManager < handle
             end
             
             %clean up DataReady listeners and plot timer
-            for listener = obj.listeners
-                delete(listener{1});
-            end
-            delete(obj.plotTimer);
+            cellfun(@delete, obj.listeners);
+            delete(obj.plotScopeTimer);
         end
         
         %Initialize
@@ -61,21 +58,20 @@ classdef ExpManager < handle
             % FIX ME
             %            header = struct();
             %            obj.dataFileHandler.open(header);
-            
-            %Connect measurment data to processing callback
-            obj.listeners{1} = addlistener(obj.scopes{1}, 'DataReady', @obj.process_data);
-            obj.plotTimer = timer('TimerFcn', @obj.plot_callback, 'Period', 0.5, 'ExecutionMode', 'fixedSpacing');
         end
         
         %Runner
         function run(obj)
+            %Connect measurment data to processing callbacks
+            obj.listeners{1} = addlistener(obj.scopes{1}, 'DataReady', @obj.process_data);
+            obj.plotScopeTimer = timer('TimerFcn', @obj.plot_scope_callback, 'StopFcn', @obj.plot_scope_callback, 'Period', 0.5, 'ExecutionMode', 'fixedSpacing');
             
             %Set the cleanup function so that even if we ctrl-c out we
             %correctly cleanup
             c = onCleanup(@() obj.cleanUp());
             
             %Start the plot timer
-            start(obj.plotTimer);
+            start(obj.plotScopeTimer);
             
             %Loop over all the sweeps
             idx = 1;
@@ -92,6 +88,20 @@ classdef ExpManager < handle
                         idx = idx + 1;
                     else % inner most loop... take data
                         obj.take_data();
+                        % pull data out of measurements
+                        stepData = structfun(@(m) m.get_data(), obj.measurements, 'UniformOutput', false);
+                        for measName = fieldnames(stepData)'
+                            % what we want to do is:
+                            % obj.data.(measNames{ct})(ct(1), ct(2), ..., ct(n), :) = stepData{ct};
+                            % lacking an idiomatic way to build the generic
+                            % assignment, we manually call subsasgn
+                            indexer = struct('type', '()', 'subs', {[num2cell(ct), ':']});
+                            if ~isfield(obj.data, measName{1})
+                                obj.data.(measName{1}) = nan([stops, length(stepData.(measName{1}))]);
+                            end
+                            obj.data.(measName{1}) = subsasgn(obj.data.(measName{1}), indexer, stepData.(measName{1}));
+                        end
+                        obj.plot_data()
                     end
                 else
                     ct(idx) = 1;
@@ -104,12 +114,10 @@ classdef ExpManager < handle
         function cleanUp(obj)
             % perform any task necessary to clean up (e.g. stop AWGs, turn
             % of uW sources, etc.)
-            stop(obj.plotTimer);
-            delete(obj.plotTimer);
+            stop(obj.plotScopeTimer);
+            delete(obj.plotScopeTimer);
             %clean up DataReady listeners and plot timer
-            for listener = obj.listeners
-                delete(listener{1});
-            end
+            cellfun(@delete, obj.listeners);
             % close data file
             obj.dataFileHandler.close();
         end
@@ -145,7 +153,51 @@ classdef ExpManager < handle
             structfun(@(m) apply(m, data), obj.measurements, 'UniformOutput', false);
         end
         
-        function plot_callback(obj, ~, ~)
+        function plot_data(obj)
+           %Plot the accumulated swept data
+            %We keep track of figure handles to not pop new ones up all the
+            %time
+            persistent figHandles axesHandles plotHandles 
+            if isempty(figHandles)
+                figHandles = struct();
+                axesHandles = struct();
+                plotHandles = struct();
+            end
+            
+            for measName = fieldnames(obj.data)'
+                measData = squeeze(obj.data.(measName{1}));
+                
+                if ~isempty(measData)
+                    if ~isfield(figHandles, measName{1}) || ~ishandle(figHandles.(measName{1}))
+                        figHandles.(measName{1}) = figure('HandleVisibility', 'callback', 'NumberTitle', 'off', 'Name', [measName{1} ' - Data']);
+                    end
+                    figHandle = figHandles.(measName{1});
+
+                    if ~isfield(axesHandles, [measName{1} '_abs']) || ~isfield(plotHandles, [measName{1} '_abs'])
+                        axesHandles.([measName{1} '_abs']) = subplot(2,1,1, 'Parent', figHandle);
+                        axesHandles.([measName{1} '_phase']) = subplot(2,1,2, 'Parent', figHandle);
+                        if length(setdiff(size(measData), 1)) == 1
+                            plotHandles.([measName{1} '_abs']) = plot(axesHandles.([measName{1} '_abs']), abs(measData));
+                            plotHandles.([measName{1} '_phase']) = plot(axesHandles.([measName{1} '_phase']), (180/pi)*angle(measData));
+                        elseif length(setdiff(size(measData), 1)) == 2
+                            plotHandles.([measName{1} '_abs']) = imagesc(abs(measData), 'Parent', axesHandles.([measName{1} '_abs']));
+                            plotHandles.([measName{1} '_phase']) = imagesc((180/pi)*angle(measData), 'Parent', axesHandles.([measName{1} '_phase']));
+                        end
+                    else
+                        if length(setdiff(size(measData), 1)) == 1
+                            set(plotHandles.([measName{1} '_abs']), 'YData', abs(measData));
+                            set(plotHandles.([measName{1} '_phase']), 'YData', (180/pi)*angle(measData));
+                        elseif length(setdiff(size(measData), 1)) == 2
+                            set(plotHandles.([measName{1} '_abs']), 'CData', abs(measData));
+                            set(plotHandles.([measName{1} '_phase']), 'CData', (180/pi)*angle(measData));
+                        end
+                    end
+                end
+            end
+            drawnow()
+        end
+        
+        function plot_scope_callback(obj, ~, ~)
             %We keep track of figure handles to not pop new ones up all the
             %time
             persistent figHandles axesHandles plotHandles 
