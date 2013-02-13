5005e74ecf60226c77a2599bb3d49baa94880d17
Coherence Controller <qlab@bbn.com>
2013-02-13 13:44:14 -0500

    Fix up the Repeat sweeper for the new setup
    --CAR and BRJ

--- common/+sweeps/Repeat.m
+++ common/+sweeps/Repeat.m
@@ -17,7 +17,6 @@
 % limitations under the License.
 classdef Repeat < sweeps.Sweep
 	properties
-        numSegments
 	end
 	
 	methods
@@ -25,16 +24,10 @@ classdef Repeat < sweeps.Sweep
 		function obj = Repeat(sweepParams, ~)
 			obj.label = 'Repeat';
 			
-			% generate time points
-			start = 1;
-			step = 1;
-            stop = sweepParams.stop;
-
-			obj.points = start:step:stop;
-            obj.numSteps = length(obj.points);
+			% generate sweep points
+            obj.numSteps = sweepParams.numRepeats;
+			obj.points = 1:obj.numSteps;
 			
-			obj.plotRange.start = start;
-			obj.plotRange.end = stop;
 		end
 		
 		% repeat stepper
