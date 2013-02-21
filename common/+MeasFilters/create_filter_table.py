1383b8b07e5731338a8b38b03b2adcf50a9af7a9
Colm Ryan <cryan@bbn.com>
2013-02-20 21:08:08 -0500

    Update filter coefficients to actually be a butterworth filter.

--- common/+MeasFilters/create_filter_table.py
+++ common/+MeasFilters/create_filter_table.py
@@ -3,7 +3,7 @@ Created on Tue Mar 27 14:34:40 2012
 
 @author: Colm Ryan 
 
-Creates a table of low-pass Butterworth filters for demodulating signal.
+Creates a table of low-pass filter coefficients for demodulating signal.
 
 We design for a pass band of 1/2 the IF frequency and a stop-band of the IF frequency.
 We arbitrarily accept 3dB of loss in the pass band and want 30dB of suppression in the stop band.
@@ -15,7 +15,7 @@ import numpy as np
 aCoeffList = []
 bCoeffList = []
 for IFFreq in np.arange(0.01, 1, 0.01):
-    (b,a) = scipy.signal.iirdesign(IFFreq/2, IFFreq, 3, 30, ftype='cheby2')
+    (b,a) = scipy.signal.iirdesign(IFFreq/2, IFFreq, 3, 30, ftype='butter')
     aCoeffList.append(a)
     bCoeffList.append(b)
 
