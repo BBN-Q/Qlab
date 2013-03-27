2722766621feffae5100504b29a2a3f4e7900e3d
Brian Donovan <bdonovan@bbn.com>
2013-03-27 13:13:38 -0400

    Added Console Thunker examples from Malibu library

---
+++ hardware/APS/libx6-cpp/src/innovative/CommandLine_Con.h
@@ -0,0 +1,21 @@
+// CommandLine_Qt.h
+//
+//    INNOVATIVE INTEGRATION CORPORATION PROPRIETARY INFORMATION
+//  This software is supplied under the terms of a license agreement or nondisclosure
+//  agreement with Innovative Integration Corporation and may not be copied or
+//  disclosed except in accordance with the terms of that agreement.
+//  Copyright (c) 2000..2005 Innovative Integration Corporation.
+//  All Rights Reserved.
+//
+
+#ifndef CommandLine_ConH
+#define CommandLine_ConH
+
+#include <CommandLineIntf_Mb.h>
+
+namespace   Innovative
+{
+void SetCommandLineArguments(int argc, char* argv[]);
+}
+
+#endif
