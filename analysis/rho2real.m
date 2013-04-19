9f8afc692f84afc90c25c20e4f82e5f688afa723
Coherence Controller <qlab@bbn.com>
2013-04-18 22:07:30 -0400

    Bug-fix: actually use transformation from paper
    --CAR

--- analysis/rho2real.m
+++ analysis/rho2real.m
@@ -11,31 +11,31 @@ n = log2(dim);
 xBasis = cell(dim,1);
 yBasis = cell(dim,1);
 
-X = [0,1;1,0]; Y = [0,-1i;1i,0]; I = eye(2);
+X = [0,1;1,0]; Y = [0,-1i;1i,0]; Z = [1,0;0,-1]; I = eye(2);
+pauliBasis = {I, Y; X, Z};
 
-for ct = 1:dim
-    binvec = bitget(ct-1, 1:n);
-    xBasis{ct} = 1;
-    yBasis{ct} = 1;
-    for ct2 = 1:n
-        if binvec(ct2)
-            xBasis{ct} = kron(X, xBasis{ct});
-            yBasis{ct} = kron(Y, yBasis{ct});
-        else
-            xBasis{ct} = kron(I, xBasis{ct});
-            yBasis{ct} = kron(I, yBasis{ct});
+
+    function pauli = get_pauli(vec1, vec2)
+        pauli = 1;
+        for myct = 1:length(vec1)
+            pauli = kron(pauli, pauliBasis{vec1(myct)+1, vec2(myct)+1});
         end
     end
-end
+
 
 out = zeros(dim,dim);
 
-for rowct = 1:dim
-    for colct = 1:dim
-        tmpMat = xBasis{rowct}*yBasis{colct};
-        %Hack to get rid of angle
-        %This can be handled with a proper multiplication rule
-        tmpMat = exp(-1j*angle(tmpMat(1,1)))*tmpMat;
-        out(rowct,colct) = real(trace(rho*tmpMat));
-    end
+M = 2^n-1;
+for ct = 0:M
+    out = out + get_pauli(bitget(ct, n:-1:1), zeros(1,n)) * rho * get_pauli(bitget(M, n:-1:1), bitget(M-ct, n:-1:1));
+end
+
+
+Q = 1;
+for ct = 1:n
+    Q = kron(Q, [1, -1i; 1 ,1]);
+end
+
+out = real(Q.*out);
+
 end
