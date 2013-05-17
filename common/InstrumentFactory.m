561d850afc180e4d6294c0a3b0cb2a8834b829c0
Coherence Controller <qlab@bbn.com>
2013-05-16 20:52:33 -0400

    Update to matlab-json

--- common/InstrumentFactory.m
+++ common/InstrumentFactory.m
@@ -1,6 +1,6 @@
 function device = InstrumentFactory(name)
     %load the instrument library
-    instrLibrary = jsonlab.loadjson(getpref('qlab', 'InstrumentLibraryFile'));
+    instrLibrary = json.read(getpref('qlab', 'InstrumentLibraryFile'));
 
     %Pull out the instrument settings dictionary
     instrSettings = instrLibrary.instrDict.(name);
