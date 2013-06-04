d47abd4963e728dab4c9a7cb7c875deff78fefcd
Coherence Controller <qlab@bbn.com>
2013-06-04 14:23:38 -0400

    Add fit of cavity line to extract cavity rate \kappa
    Hat-tip BRJ for fitting function.
    
    --CAR

--- experiments/muWaveDetection/cavity_power_scan.m
+++ experiments/muWaveDetection/cavity_power_scan.m
@@ -1,4 +1,4 @@
-function [rawData, delayedPhases, freqs] = cavity_power_scan(na, powerScan, fixedAtten)
+function [rawData, delayedPhases, freqs, kappa] = cavity_power_scan(na, powerScan, fixedAtten)
 %cavity_power_scan Scan the power of the network analyser to find the bare
 %to dressed shift 
 %
@@ -18,9 +18,9 @@ phases = unwrap(angle(rawData), [], 2);
 
 delayedPhases = zeros(size(phases));
 
-fitCoeffs = polyfit(freqs(1:100), phases(1,1:100),1);
+fitCoeffs = polyfit(freqs(1:100)/1e9, phases(1,1:100),1);
 for ct = 1:size(phases,1)
-    delayedPhases(ct,:) = phases(ct,:) - polyval(fitCoeffs, freqs);
+    delayedPhases(ct,:) = phases(ct,:) - polyval(fitCoeffs, freqs/1e9);
 end
 
 %magic number cutoff to try and catch where unwrap goes back in between the
@@ -31,5 +31,15 @@ figure()
 imagesc(freqs/1e9, powerScan-fixedAtten, delayedPhases);
 xlabel('Frequency (GHz)');
 ylabel('Drive Power (dBm)');
+
+%Fit the lower power to extract the cavity \Kappa
+modelF = @(beta, f) 2*beta(1)*atan2(f - beta(2), beta(3)/2) + beta(4);
+%Fit it MHz
+[~, peakIndex] = min(diff(unwrap(delayedPhases(1,:))));
+betas = nlinfit(freqs/1e6, unwrap(delayedPhases(1,:)), modelF, [-1, freqs(peakIndex)/1e6, 1, -3]);
+kappa = betas(3);
+text(freqs(end/4)/1e9, powerScan(1)-fixedAtten+5, ['$\kappa = ', num2str(kappa, '%1.2f'), ' MHz$'], 'Color', 'white', 'interpreter', 'latex', 'FontSize', 14)
+
+end
 
 
