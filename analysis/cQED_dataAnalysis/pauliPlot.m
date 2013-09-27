290780026f23574b1ffa0a879caf57735273bd81
Blake R. Johnson <bjohnson@bbn.com>
2013-09-27 13:37:47 -0400

    Update pauliPlot to work with more qubits, or sort operators by Hamming weight.

--- analysis/cQED_dataAnalysis/pauliPlot.m
+++ analysis/cQED_dataAnalysis/pauliPlot.m
@@ -1,15 +1,30 @@
-function pauliPlot(A)
-    [~, pauliStrs] = paulis(1);
+function pauliPlot(A, nbrQubits)
+    [~, pauliStrs] = paulis(nbrQubits);
+    % sort by "hamming" weight of the pauli operator
+    weights = cellfun(@pauliHamming, pauliStrs);
+    [~, weightIdx] = sort(weights);
+    pauliStrs = pauliStrs(weightIdx);
+    A = A(weightIdx, weightIdx);
     cmap = [hot(50); 1-hot(50)];
     cmap = cmap(18:18+63,:); % make a 64-entry colormap
     figure()
     imagesc(A,[-1,1])
     colormap(cmap)
     colorbar()
-    set(gca, 'XTick', 1:4);
+    set(gca, 'XTick', 1:4^nbrQubits);
     set(gca, 'XTickLabel', pauliStrs);
-    set(gca, 'YTick', 1:4);
+    set(gca, 'YTick', 1:4^nbrQubits);
     set(gca, 'YTickLabel', pauliStrs);
     xlabel('Input Pauli Operator');
     ylabel('Output Pauli Operator');
 end
+
+function weight = pauliHamming(str)
+    weight = 0;
+    for pauli = str
+        if ~strcmp(pauli, 'I')
+            weight = weight + 1;
+        end
+    end
+end
+    
