3ba177ecec41a805395c7552dd176be00e0304df
Coherence Controller <qlab@bbn.com>
2013-10-22 14:14:48 -0400

    Give bgsubtract some bsxfun love
    --CAR

--- analysis/bgsubtract.m
+++ analysis/bgsubtract.m
@@ -3,6 +3,6 @@ function outMat = bgsubtract(data)
 % Subtracts the mean of every row from a 2D data set
 % Returns the subtracted set.
 
-outMat = data - repmat(mean(data,2),[1, size(data,2)]);
+outMat = bsxfun(@minus, data, mean(data,2));
 
 end
