af3739ec91952e6585fac9a1a844b70ba4b365fd
Coherence Controller <qlab@bbn.com>
2013-11-14 09:32:02 -0500

    cal_scale handles 2D data
    Other cleanups too.
    
    --CAR

--- analysis/cQED_dataAnalysis/cal_scale.m
+++ analysis/cQED_dataAnalysis/cal_scale.m
@@ -1,5 +1,5 @@
 function data = cal_scale(varargin)
-    % FUNCTION calScale(dupFactor, handle h)
+    % FUNCTION cal_scale(varargin)
     % Rescales the data by the pi and 0 calibration experiments at the end.
     % Inputs:
     % (data/plotGrab) - raw data set to calibrate or choose the "one", "top" or "bottom" to grab from current figure 
@@ -8,8 +8,7 @@ function data = cal_scale(varargin)
     %Check if we have data in or whether we are grabbing from a plot
     if isnumeric(varargin{1})
         data = varargin{1};
-        xpts = varargin{2};
-        if nargin < 3
+        if nargin < 2
             numRepeats = 2;
         else
             numRepeats = varargin{2};
@@ -18,9 +17,9 @@ function data = cal_scale(varargin)
         axesHs = findobj(gcf, 'Type', 'Axes');
         switch varargin{1}
             case 'bottom'
-                axesH = axesHs(1);
-            case {'top','one'}
                 axesH = axesHs(2);
+            case {'top','one'}
+                axesH = axesHs(1);
             otherwise
                 error('Unknown plot grab command.');
         end
@@ -39,35 +38,48 @@ function data = cal_scale(varargin)
         error('First argument should be data or plotGrab string.')
     end
     
-    
     % extract calibration experiments
-    zeroCal = mean(data(end-2*numRepeats+1:end-numRepeats));
-    piCal = mean(data(end-numRepeats+1:end));
-    scaleFactor = (piCal - zeroCal)/2;
+    switch nsdims(data)
+        case 1
+            zeroCal = mean(data(end-2*numRepeats+1:end-numRepeats));
+            piCal = mean(data(end-numRepeats+1:end));
+            scaleFactor = (piCal - zeroCal)/2;
+            data = data(1:end-2*numRepeats);
+            data = (data - zeroCal)./scaleFactor - 1;
+    
+        case 2
+            zeroCal = mean(data(:, end-2*numRepeats+1:end-numRepeats), 2);
+            piCal = mean(data(:, end-numRepeats+1:end), 2);
+            scaleFactor = (piCal - zeroCal)/2;
 
-    xpts = xpts(1:end-2*numRepeats);
-    data = data(1:end-2*numRepeats);
-    
-    % rescale
-    data = (data - zeroCal)./scaleFactor - 1;
-    
-    % save axis labels and figure title
-    xlab = get(get(axesH, 'XLabel'), 'String');
-    ylab = get(get(axesH, 'YLabel'), 'String');
-    figTitle = get(get(axesH, 'Title'), 'String');
-    
-    % create the new plot
-    figure()
-    plot(xpts, data);
-    set(gca, 'YDir', 'normal');
-    if ~isempty(xlab)
-        xlabel(xlab)
+            data = data(:, 1:end-2*numRepeats);
+            data = bsxfun(@rdivide, bsxfun(@minus, data, zeroCal), scaleFactor) - 1;
+            
+        otherwise
+            error('Unable to handle dimensions of data')
     end
-    if ~isempty(ylab)
-        ylabel(ylab)
-    end
-    if ~isempty(figTitle)
-        title(figTitle)
+    
+    if exist('axesH', 'var')
+        xpts = xpts(1:end-2*numRepeats);
+
+        % save axis labels and figure title
+        xlab = get(get(axesH, 'XLabel'), 'String');
+        ylab = get(get(axesH, 'YLabel'), 'String');
+        figTitle = get(get(axesH, 'Title'), 'String');
+
+        % create the new plot
+        figure()
+        plot(xpts, data);
+        set(gca, 'YDir', 'normal');
+        if ~isempty(xlab)
+            xlabel(xlab)
+        end
+        if ~isempty(ylab)
+            ylabel(ylab)
+        end
+        if ~isempty(figTitle)
+            title(figTitle)
+        end
     end
     
 end
