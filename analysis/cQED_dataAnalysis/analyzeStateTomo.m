c50ac8cf15e7a96eca56181ba6208efa277a050e
Coherence Controller <qlab@bbn.com>
2013-12-18 12:17:40 -0500

    Update the state tomography wrapper
    New inversion routines and data layout

--- analysis/cQED_dataAnalysis/analyzeStateTomo.m
+++ analysis/cQED_dataAnalysis/analyzeStateTomo.m
@@ -1,37 +1,79 @@
-function [beta, raw_paulis, C] = analyzeStateTomo(data, nbrQubits, nbrRepeats)
-%analyzeStateTomo Wrapper function for state tomography.
+function [beta, raw_paulis, C] = analyzeStateTomo(data, nbrQubits, nbrPulses, nbrCalRepeats)
+% analyzeStateTomo Wrapper function for state tomography.
 %
-% [beta, paulis, C] = analyzeStateTomo(data, nbrQubits, nbrRepeats)
+% [rhoLSQ, rhoSDP, rhoWizard] = analyzeStateTomo(data, nbrQubits, nbrPulses, nbrCalRepeats)
 %
-% Expects calibration data then tomography data
+% Expects tomography data then calibration data
 
-calData = mean(reshape(data(1:nbrRepeats*2^nbrQubits), nbrRepeats, 2^nbrQubits), 1);
-tomoData = mean(reshape(data(nbrRepeats*2^nbrQubits+1:end), nbrRepeats, 4^nbrQubits), 1);
+%First separate out the data
 
-[beta raw_paulis] = PauliTomo(calData, tomoData);
-figure;
-bar(raw_paulis)
-PauliLabel();
-title('Raw Inversion Pauli Decomposition');
+%Average over calibration repeats
+calData = mean(reshape(data(end-nbrCalRepeats*(2^nbrQubits)+1:end), nbrCalRepeats, 2^nbrQubits), 1);
 
-rhoRaw = getRho(raw_paulis);
-rhoWizard = WizardTomo_(rhoRaw, nbrQubits);
-rhoPlot(rhoWizard);
-purity = real(trace(rhoWizard^2))
+%Pull out the calibrations as diagonal measurement operators
+measOps = cell(1,1);
+measOps{1} = diag(calData);
 
-pauliLabels = {'II', 'XI', 'YI', 'ZI', 'IX', 'IY', 'IZ', 'XY', 'XZ', 'YX', 'YZ', 'ZX', 'ZY', 'XX', 'YY', 'ZZ'};
-[pauliOps, pauliStrs] = paulis(nbrQubits);
+%The data to invert
+tomoData = data(1:end-nbrCalRepeats*(2^nbrQubits));
 
-paulisWiz = zeros(16,1);
-for ct = 1:length(pauliOps)
-    pauliNum = find(strcmp(pauliLabels{ct}, pauliStrs));
-    paulisWiz(ct) = real(trace(rhoWizard*pauliOps{pauliNum}));
+%Map each experiment to the appropriate readout pulse 
+measPulseMap = 1:nbrPulses^nbrQubits;
+measOpMap = ones(36,1);
+
+%Use a helper to get the measurement unitaries.
+measPulseUs = tomo_gate_set(nbrQubits, nbrPulses);
+
+%TODO: handle variances
+varMat = diag(ones(nbrPulses^nbrQubits,1));
+
+%Now call the inversion routines
+
+%First least squares
+rhoLSQ = QST_LSQ(tomoData, varMat, measPulseMap, measOpMap, measPulseUs, measOps, nbrQubits);
+
+function pauli_label()
+    [~, pauliStrs] = paulis(nbrQubits);
+    set(gca(), 'XTick', 1:4^nbrQubits); 
+    set(gca(), 'XTickLabel', pauliStrs);
 end
-figure;
-bar(paulisWiz)
-PauliLabel()
-title('Constrained Tomography Pauli Decomposition','FontSize',14);
-xlabel('Pauli Operator','FontSize',12);
-ylabel('Overlap','FontSize',12);
 
-C = Concurrence_(rhoWizard);
+figure()
+subplot(2,1,1)
+bar(rho2pauli(rhoLSQ));
+pauli_label();
+title('LSQ Inversion Pauli Decomposition');
+
+
+%Now constrained SDP
+rhoSDP = QST_SDP_uncorrelated(tomoData, varMat, measPulseMap, measOpMap, measPulseUs, measOps, nbrQubits);
+
+subplot(2,1,2)
+bar(rho2pauli(rhoSDP));
+pauli_label();
+title('SDP Inversion Pauli Decomposition');
+
+
+% rhoRaw = getRho(raw_paulis);
+% rhoWizard = WizardTomo_(rhoRaw, nbrQubits);
+% rhoPlot(rhoWizard);
+% purity = real(trace(rhoWizard^2))
+% 
+% pauliLabels = {'II', 'XI', 'YI', 'ZI', 'IX', 'IY', 'IZ', 'XY', 'XZ', 'YX', 'YZ', 'ZX', 'ZY', 'XX', 'YY', 'ZZ'};
+% [pauliOps, pauliStrs] = paulis(nbrQubits);
+% 
+% paulisWiz = zeros(16,1);
+% for ct = 1:length(pauliOps)
+%     pauliNum = find(strcmp(pauliLabels{ct}, pauliStrs));
+%     paulisWiz(ct) = real(trace(rhoWizard*pauliOps{pauliNum}));
+% end
+% figure;
+% bar(paulisWiz)
+% PauliLabel()
+% title('Constrained Tomography Pauli Decomposition','FontSize',14);
+% xlabel('Pauli Operator','FontSize',12);
+% ylabel('Overlap','FontSize',12);
+% 
+% C = Concurrence_(rhoWizard);
+
+end
