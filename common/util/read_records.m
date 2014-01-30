502b1919fbb6862111d15b8be3c8a64ed0f52fa5
CoherenceController@Draco <qlab@bbn.com>
2014-01-30 13:50:22 -0500

    Don't use an unnecessary closure in read_records().
    --BRJ

--- common/util/read_records.m
+++ common/util/read_records.m
@@ -3,18 +3,16 @@ function records = read_records(fileName)
 %
 % records = read_records(fileName)
 
-%Open the real and imaginary files and then combine
-    function data = read_file(myFileName)
-        fid = fopen(myFileName, 'rb');
-        sizes = fread(fid,3, 'int32');
-        data = fread(fid, inf, 'single=>single');
-        lastDim = length(data)/prod(sizes);
-        data = reshape(data, [sizes; lastDim]');
-        fclose(fid);
-    end
-
+    %Open the real and imaginary files and then combine
     records = read_file([fileName, '.real']) +1j*read_file([fileName, '.imag']);
 end
 
-
+function data = read_file(myFileName)
+    fid = fopen(myFileName, 'rb');
+    sizes = fread(fid,3, 'int32');
+    data = fread(fid, inf, 'single=>single');
+    lastDim = length(data)/prod(sizes);
+    data = reshape(data, [sizes; lastDim]');
+    fclose(fid);
+end
 
