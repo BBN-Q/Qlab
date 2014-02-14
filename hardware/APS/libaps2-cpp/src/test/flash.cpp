851e9e63b9b3b3d8f5dd5057808e0231ff6bb078
CoherenceController <qlab@bbn.com>
2014-02-14 11:36:00 -0500

    Add flash read/write test.
    --BRJ

---
+++ hardware/APS/libaps2-cpp/src/test/flash.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+
+#include "headings.h"
+#include "libaps.h"
+#include "constants.h"
+
+#include <concol.h> 
+
+using namespace std;
+
+// command options functions taken from:
+// http://stackoverflow.com/questions/865668/parse-command-line-arguments
+string getCmdOption(char ** begin, char ** end, const std::string & option)
+{
+  char ** itr = std::find(begin, end, option);
+  if (itr != end && ++itr != end)
+  {
+    return string(*itr);
+  }
+  return "";
+}
+
+bool cmdOptionExists(char** begin, char** end, const std::string& option)
+{
+  return std::find(begin, end, option) != end;
+}
+
+
+int main (int argc, char* argv[])
+{
+
+  concol::concolinit();
+  cout << concol::RED << "BBN AP2 Flash Test Executable" << concol::RESET << endl;
+
+
+  int dbgLevel = 8;
+  if (argc >= 2) {
+    dbgLevel = atoi(argv[1]);
+  }
+
+  set_logging_level(dbgLevel);
+  
+  cout << concol::RED << "Attempting to initialize libaps" << concol::RESET << endl;
+
+  init("eth0");
+
+  int numDevices = get_numDevices();
+
+  cout << concol::RED << numDevices << " APS device" << (numDevices > 1 ? "s": "")  << " found" << concol::RESET << endl;
+
+  if (numDevices < 1)
+  	return 0;
+  
+  cout << concol::RED << "Attempting to get serials" << concol::RESET << endl;
+
+  const char ** serialBuffer = new const char*[numDevices];
+  get_deviceSerials(serialBuffer);
+
+  for (int cnt=0; cnt < numDevices; cnt++) {
+  	cout << concol::RED << "Device " << cnt << " serial #: " << serialBuffer[cnt] << concol::RESET << endl;
+  }
+
+  cout << concol::RED << "Connecting to device serial #: " << serialBuffer[0] << concol::RESET << endl;
+
+  string deviceSerial(serialBuffer[0]);
+
+  connect_APS(deviceSerial.c_str());
+
+  double uptime = get_uptime(deviceSerial.c_str());
+
+  cout << concol::RED << "Uptime for device " << deviceSerial << " is " << uptime << " seconds" << concol::RESET << endl;
+
+  uint32_t buffer[2] = {0, 0};
+
+  // test reading from FLASH
+  read_flash(deviceSerial.c_str(), 0x00FF0000, 2, buffer);
+  cout << "Programmed MAC address at 0x00FF0000 is " << endl << myhex << buffer[0] << " " << buffer[1] << endl;
+
+  // read SPI setup sequence
+  uint32_t setup[32];
+  read_flash(deviceSerial.c_str(), 0x0, 32, setup);
+  cout << "Programmed setup SPI sequence:" << endl;
+  for (size_t ct=0; ct < 32; ct++) {
+    cout << myhex << setup[ct] << " ";
+    if (ct % 4 == 3) cout << endl;
+  }
+
+  // write new SPI setup sequence
+  // write_SPI_setup(deviceSerial.c_str());
+
+  cout << concol::RED << "Reading flash addr 0x00FA0000" << concol::RESET << endl;
+  read_flash(deviceSerial.c_str(), 0x00FA0000, 2, buffer);
+  cout << "Received " << myhex << buffer[0] << " " << buffer[1] << endl;
+
+  cout << concol::RED << "Erasing/writing flash addr 0x00FA0000" << concol::RESET << endl;
+  buffer[0] = 0xBADDF00F;
+  write_flash(deviceSerial.c_str(), 0x00FA0000, buffer, 1);
+
+  cout << concol::RED << "Reading flash addr 0x00FA0000" << concol::RESET << endl;
+  read_flash(deviceSerial.c_str(), 0x00FA0000, 2, buffer);
+  cout << "Received " << myhex << buffer[0] << " " << buffer[1] << endl;
+  
+  disconnect_APS(deviceSerial.c_str());
+
+  delete[] serialBuffer;
+  
+  cout << concol::RED << "Finished!" << concol::RESET << endl;
+  
+  return 0;
+}
