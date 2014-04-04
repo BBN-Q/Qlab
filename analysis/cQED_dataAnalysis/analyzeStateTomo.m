459720c479c989da3ca423ed611d114944e9a12d
CoherenceController@Draco <qlab@bbn.com>
2014-04-03 21:22:36 -0400

    Attempt to generalize state tomography for multi-channel data
    --CAR and TAO

--- analysis/cQED_dataAnalysis/analyzeStateTomo.m
+++ analysis/cQED_dataAnalysis/analyzeStateTomo.m
@@ -1,4 +1,4 @@
-function [beta, raw_paulis, C] = analyzeStateTomo(data, nbrQubits, nbrPulses, nbrCalRepeats)
+function [rhoLSQ, rhoSDP] = analyzeStateTomo(datastruct, nbrQubits, nbrPulses, nbrCalRepeats)
 % analyzeStateTomo Wrapper function for state tomography.
 %
 % [rhoLSQ, rhoSDP, rhoWizard] = analyzeStateTomo(data, nbrQubits, nbrPulses, nbrCalRepeats)
@@ -6,26 +6,36 @@ function [beta, raw_paulis, C] = analyzeStateTomo(data, nbrQubits, nbrPulses, nb
 % Expects tomography data then calibration data
 
 %First separate out the data
+data=datastruct.data;
+numMeas = length(data);
+measOps = cell(numMeas,1);
+tomoData = [];
+varData = [];
 
-%Average over calibration repeats
-calData = mean(reshape(data(end-nbrCalRepeats*(2^nbrQubits)+1:end), nbrCalRepeats, 2^nbrQubits), 1);
+for ct = 1:numMeas
+    data{ct} = real(data{ct});
+    %Average over calibration repeats
+    calData = mean(reshape(data{ct}(end-nbrCalRepeats*(2^nbrQubits)+1:end), nbrCalRepeats, 2^nbrQubits), 1);
 
-%Pull out the calibrations as diagonal measurement operators
-measOps = cell(1,1);
-measOps{1} = diag(calData);
+    %Pull out the calibrations as diagonal measurement operators
+    measOps{ct} = diag(calData);
 
-%The data to invert
-tomoData = data(1:end-nbrCalRepeats*(2^nbrQubits));
-
+    %The data to invert
+    tomoData = [tomoData; data{ct}(1:end-nbrCalRepeats*(2^nbrQubits))];
+    
+    %variance
+    varData = [varData; datastruct.realvar{ct}(1:end-nbrCalRepeats*(2^nbrQubits))];
+        
+end
 %Map each experiment to the appropriate readout pulse 
-measPulseMap = 1:nbrPulses^nbrQubits;
-measOpMap = ones(36,1);
+measOpMap = reshape(repmat(1:numMeas, nbrPulses^nbrQubits, 1), numMeas*(nbrPulses^nbrQubits), 1); 
+measPulseMap = repmat([1:nbrPulses^nbrQubits]', numMeas, 1);
 
 %Use a helper to get the measurement unitaries.
 measPulseUs = tomo_gate_set(nbrQubits, nbrPulses);
 
 %TODO: handle variances
-varMat = diag(ones(nbrPulses^nbrQubits,1));
+varMat = diag(varData);
 
 %Now call the inversion routines
 
