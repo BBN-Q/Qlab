BBN-Q/Qlab

commit eaa9ef656a601e60d03dce3ae5d435582f211a81

Allow InstrumentFactory to import from outside the deviceDrivers package.

common/InstrumentFactory.m
@@ -11,8 +11,9 @@ function device = InstrumentFactory(name, instrSettings)
     else
         deviceClass = instrSettings.deviceName;
     end
-        
-    device = deviceDrivers.(deviceClass);
+
+    import deviceDrivers.*
+    device = eval(name);
     device.connect(instrSettings.address);
 
 end

commit 7ce6fe38886f9b335038657294db07f30b2ce9fe

Fix regression introduced by eaa9ef6.
--BRJ

common/InstrumentFactory.m
@@ -13,7 +13,7 @@ function device = InstrumentFactory(name, instrSettings)
     end
 
     import deviceDrivers.*
-    device = eval(name);
+    device = eval(deviceClass);
     device.connect(instrSettings.address);
 
 end