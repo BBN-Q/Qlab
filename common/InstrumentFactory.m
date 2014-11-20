9b2238006e490edc5c681003b23f1410932e2416
CoherenceController <qlab@bbn.com>
2014-11-20 16:37:32 -0500

    Suppress some annoying JSON warning.
    Matlab structure fields have specific requirements
    
    --CAR

--- common/InstrumentFactory.m
+++ common/InstrumentFactory.m
@@ -3,7 +3,11 @@ function device = InstrumentFactory(name, instrSettings)
     %If we weren't passed settings then load from library
     if ~exist('instrSettings', 'var')
         %load the instrument library
+        %turn off annoying warnings about illegal Matlab structure field
+        %names
+        warning('off', 'json:fieldNameConflict');
         instrLibrary = json.read(getpref('qlab', 'InstrumentLibraryFile'));
+        warning('on', 'json:fieldNameConflict');
 
         %Pull out the instrument settings dictionary
         instrSettings = instrLibrary.instrDict.(name);
