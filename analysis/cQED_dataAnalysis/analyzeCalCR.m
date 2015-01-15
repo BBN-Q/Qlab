6584d19a6b64436510cbfaa7f1c2d7746d171f35
CoherenceController <qlab@bbn.com>
2015-01-15 18:43:37 -0500

    Included CR length and phase calibration
    only simple analysis. Script to run the measurements needs to be added
    --DR

--- analysis/cQED_dataAnalysis/analyzeCalCR.m
+++ analysis/cQED_dataAnalysis/analyzeCalCR.m
@@ -1,8 +1,13 @@
-function optphase = analyzeCalCR(CRdata,channel)
-%simple fit function to get optimum phase in a CR calibration
+function analyzeCalCR(caltype, CRdata,channel)
+%simple fit function to get optimum length/phase in a CR calibration
 %and set in pulse params
 
+%caltype: 
+% 1 - length
+% 2 - phase
+
 %channel: X6 channel with the target data (1 or 2)
+
 %dataSource = 'II_1_1_1';
 
 CRpulsename = 'CR12';
@@ -14,31 +19,58 @@ data = cal_scale(data,4); %num repeats. Assuming the calibrations are 4 |0>, the
 
 data0 = data(1:length(data)/2);
 data1 = data(length(data)/2+1:end);
+
 sinf = @(p,t) p(1)*cos(2*pi/p(2)*t+p(3))+p(4);
 p=[1,xpoints(end),0,0];
 
 %fit sine curves
-[beta0,r0,j0] = nlinfit(xpoints, data0(:),sinf,p);
-[beta1,r1,j1] = nlinfit(xpoints, data1(:),sinf,p);
+[beta0,~,~] = nlinfit(xpoints, data0(:),sinf,p);
+[beta1,~,~] = nlinfit(xpoints, data1(:),sinf,p);
+%todo: make fit xpoints finer
 yfit0 = sinf(beta0,xpoints);
 yfit1 = sinf(beta1,xpoints);
 
-%find max contrast
-ctrfit = yfit0-yfit1;
-[ctrmax, indmax] = max(ctrfit);
-optphase = xpoints(indmax);
-fprintf('Phase index for maximum contrast = %f\n', indmax)
-fprintf('Optimum phase = %f\n', optphase)
+if(caltype==1) 
+    %find the first zero crossing
+    %dyfit0 = diff(yfit0); dyfit1 = diff(yfit1);
+    %find(dyfit0>0)
+    %yfit0c = yfit0(1:
+    yfit0c = yfit0(1:round(beta0(2)/2/(xpoints(2)-xpoints(1)))); %select the first half period 
+    yfit1c = yfit1(1:round(beta1(2)/2/(xpoints(2)-xpoints(1))));
+    [~, indmin0] = min(abs(yfit0c(:)));  %min returns the index of the first zero crossing
+    [~, indmin1] = min(abs(yfit1c(:)));  %min returns the index of the first zero crossing
+    optlen = mean([xpoints(indmin0),xpoints(indmin1)]);
+    fprintf('Length index for CNOT = %f\n', mean([indmin0, indmin1])); 
+    fprintf('Optimum length = %f ns\n', optlen)
+    fprintf('Mismatch between |0> and |1> = %f ns\n', abs(xpoints(indmin1)-xpoints(indmin0)))
+elseif(caltype==2)
+    %find max contrast
+    ctrfit = yfit0-yfit1;
+    [~, indmax] = max(ctrfit);
+    optphase = xpoints(indmax);
+    fprintf('Phase index for maximum contrast = %d\n', indmax)
+    fprintf('Optimum phase = %f\n', optphase)
+else
+    frpintf('Calibration type not supported')
+    return
+end
 
 figure(getpref('plots','CRHandle'));
 plot(xpoints, data0, 'b.', xpoints, data1, 'r.', xpoints, yfit0, 'b-', xpoints, yfit1, 'r-');
 legend('ctrlQ in |0>','ctrlQ in |1>');
 ylim([-1,1]); ylabel('<Z>'); xlabel ('Phase (deg)');
 
-%update phase in CR pulse parameters
+%update length/phase in CR pulse parameters
 warning('off', 'json:fieldNameConflict');
 channelLib = json.read(getpref('qlab','ChannelParamsFile'));
 warning('on', 'json:fieldNameConflict');
 chDict = channelLib.channelDict;
-updateLengthPhase(CRpulsename, chDict.(CRpulsename).pulseParams.length, optphase);
+if(caltype==1)
+    outlen = optlen;
+    outphase = chDict.(CRpulsename).pulseParams.phase;
+else
+    outlen = chDict.(CRpulsename).pulseParams.length;
+    outphase = optphase;
+end
+updateLengthPhase(CRpulsename, outlen, outphase);
 end
