70d8d81139c61a2a10066b63e4580c40cca4687a
CoherenceController <qlab@bbn.com>
2015-01-21 14:56:52 -0500

    Fixed record saving for StreamSelector
    --BRJ and DR

--- common/+MeasFilters/StreamSelector.m
+++ common/+MeasFilters/StreamSelector.m
@@ -20,6 +20,10 @@ classdef StreamSelector < MeasFilters.MeasFilter
     properties
         channel
         stream
+        saveRecords
+        fileHandleReal
+        fileHandleImag
+        headerWritten = false;
     end
     
     methods
@@ -30,6 +34,11 @@ classdef StreamSelector < MeasFilters.MeasFilter
             % first index is the physical channel
             obj.channel = streamVec(1);
             obj.stream = streamVec;
+            obj.saveRecords = settings.saveRecords;
+            if obj.saveRecords
+                obj.fileHandleReal = fopen([settings.recordsFilePath, '.real'], 'wb');
+                obj.fileHandleImag = fopen([settings.recordsFilePath, '.imag'], 'wb');
+            end
         end
         
         function apply(obj, src, ~)
@@ -37,23 +46,23 @@ classdef StreamSelector < MeasFilters.MeasFilter
             %Pull the raw stream from the digitizer
             obj.latestData = src.transfer_stream(obj.stream(1), obj.stream(2), obj.stream(3));
             
-%             %If we have a file to save to then do so
-%             if obj.saveRecords
-%                 if ~obj.headerWritten
-%                     %Write the first three dimensions of the signal:
-%                     %recordLength, numWaveforms, numSegments
-%                     sizes = size(obj.latestData);
-%                     if length(sizes) == 2
-%                         sizes = [sizes(1), 1, sizes(2)];
-%                     end
-%                     fwrite(obj.fileHandleReal, sizes(1:3), 'int32');
-%                     fwrite(obj.fileHandleImag, sizes(1:3), 'int32');
-%                     obj.headerWritten = true;
-%                 end
-%                 
-%                 fwrite(obj.fileHandleReal, real(obj.latestData), 'single');
-%                 fwrite(obj.fileHandleImag, imag(obj.latestData), 'single');
-%             end
+%           %If we have a file to save to then do so
+            if obj.saveRecords
+                if ~obj.headerWritten
+                    %Write the first three dimensions of the signal:
+                    %recordLength, numWaveforms, numSegments
+                    sizes = size(obj.latestData);
+                    if length(sizes) == 2
+                        sizes = [sizes(1), 1, sizes(2)];
+                    end
+                    fwrite(obj.fileHandleReal, sizes(1:3), 'int32');
+                    fwrite(obj.fileHandleImag, sizes(1:3), 'int32');
+                    obj.headerWritten = true;
+                end
+                
+                fwrite(obj.fileHandleReal, real(obj.latestData), 'single');
+                fwrite(obj.fileHandleImag, imag(obj.latestData), 'single');
+            end
             
 
             %Data accumulated in driver
