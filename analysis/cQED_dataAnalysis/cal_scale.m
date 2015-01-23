baf66d52c8be685039068d24ae7f74b2fd11a1e6
CoherenceController <qlab@bbn.com>
2015-01-23 17:52:52 -0500

    Optional caltype
    normalizing with calibration points for either B or A in |BA>
    --DR

--- analysis/cQED_dataAnalysis/cal_scale.m
+++ analysis/cQED_dataAnalysis/cal_scale.m
@@ -4,6 +4,8 @@ function data = cal_scale(varargin)
     % Inputs:
     % (data/plotGrab) - raw data set to calibrate or choose the "one", "top" or "bottom" to grab from current figure 
     % (numRepeats) - number of times calibration experiments are repeated (default: 2)
+    % (caltype) - 'b': (0)^n (1)^n; 'a': (0011)^(n/2). Need to be upgraded with generic
+    % no. of qubits and repeats
     
     
     %Check if we have data in or whether we are grabbing from a plot
@@ -13,6 +15,11 @@ function data = cal_scale(varargin)
             numRepeats = 2;
         else
             numRepeats = varargin{2};
+        end
+        if nargin < 3
+            caltype = 'b';
+        else
+            caltype = varargin{3};
         end
         xlab = ''; ylab = ''; figTitle = '';
     elseif ischar(varargin{1})
@@ -33,10 +40,25 @@ function data = cal_scale(varargin)
         data = get(lineHandle(1), 'YData');
         if nargin < 2
             numRepeats = 2;
-        else
+            caltype = 'b';
+        elseif isnumeric(varargin{2})
             numRepeats = varargin{2};
+            caltype = 'b';
+        else
+            if nargin==2
+                caltype = varargin{2}; 
+                numRepeats = 2;
+            else
+                if ischar(varargin{2})
+                    caltype = varargin{2};
+                    numRepeats = varargin{3};
+                else
+                    caltype = varargin{3};
+                    numRepeats = varargin{2};
+                end
+            end
         end
-        
+               
         % save axis labels and figure title
         xlab = get(get(axesH, 'XLabel'), 'String');
         ylab = get(get(axesH, 'YLabel'), 'String');
@@ -48,15 +70,27 @@ function data = cal_scale(varargin)
     % extract calibration experiments
     switch nsdims(data)
         case 1
-            zeroCal = mean(data(end-2*numRepeats+1:end-numRepeats));
-            piCal = mean(data(end-numRepeats+1:end));
-            scaleFactor = -(piCal - zeroCal)/2;
-            data = data(1:end-2*numRepeats);
-            data = (data - zeroCal)./scaleFactor + 1;
+            switch caltype
+                case 'a'
+                    zeroCal = (mean(data(end-2*numRepeats+1:4:end))+mean(data(end-2*numRepeats+2:4:end)))/2;
+                    piCal = (mean(data(end-2*numRepeats+3:4:end))+mean(data(end-2*numRepeats+4:4:end)))/2;
+                case 'b'
+                    zeroCal = mean(data(end-2*numRepeats+1:end-numRepeats));
+                    piCal = mean(data(end-numRepeats+1:end));
+            end
+        scaleFactor = -(piCal - zeroCal)/2;
+        data = data(1:end-2*numRepeats);
+        data = (data - zeroCal)./scaleFactor + 1;
     
         case 2
-            zeroCal = mean(data(:, end-2*numRepeats+1:end-numRepeats), 2);
-            piCal = mean(data(:, end-numRepeats+1:end), 2);
+            switch caltype
+                  case 'a'
+                    zeroCal = (mean(data(:,end-2*numRepeats+1:4:end),2)+mean(data(:,end-2*numRepeats+2:4:end),2))/2;
+                    piCal = (mean(data(:,end-2*numRepeats+3:4:end),2)+mean(data(:,end-2*numRepeats+4:4:end),2))/2;
+                case 'b'
+                    zeroCal = mean(data(:,end-2*numRepeats+1:end-numRepeats),2);
+                    piCal = mean(data(:,end-numRepeats+1:end),2);
+            end
             scaleFactor = -(piCal - zeroCal)/2;
 
             data = data(:, 1:end-2*numRepeats);
