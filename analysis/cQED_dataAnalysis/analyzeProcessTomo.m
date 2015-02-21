b08ff0980cbc0f9d3960b46a37247af801441521
CoherenceController@Draco <qlab@bbn.com>
2015-02-20 21:08:57 -0500

    Update analyzeProcessTomo to work with individual readout.
    --probably CAR and BRJ (maybe MS, too?)

--- analysis/cQED_dataAnalysis/analyzeProcessTomo.m
+++ analysis/cQED_dataAnalysis/analyzeProcessTomo.m
@@ -1,41 +1,59 @@
-function [gateFidelity, choiSDP] = analyzeProcessTomo(data, idealProcess, nbrQubits, nbrPrepPulses, nbrReadoutPulses, calRepeats)
+function [gateFidelity, choiSDP, choiLSQ] = analyzeProcessTomo(data, idealProcess, nbrQubits, nbrPrepPulses, nbrReadoutPulses, nbrCalRepeats)
 %analyzeProcess Performs SDP tomography, calculates gates fidelites and plots pauli maps.
 %
 % [gateFidelity, choiSDP] = analyzeProcessTomo(data, idealProcessStr, nbrQubits, nbrPrepPulses, nbrReadoutPulses, nbrRepeats) 
 
-% seperate calibration experiments from tomography data, and reshape
-% accordingly
-%The data.abs_Data comes a matrix (numPulseSeqs X numExpsperPulseSeq) with
-%the calibration data the last 2^nbrQubits of each row.  We need to go
+%seperate calibration experiments from tomography data and flatten the
+%experiment data
+
+%The data comes in as a matrix (numSeqs X numExpsPerSeq) with
+%the calibration data the last nbrCalRepeats2^nbrQubits of each row. We need to go
 %through each column and extract the calibration data and record a map of
 %which measurement operator each experiment corresponds to.
 
-numPreps = nbrPrepPulses^nbrQubits;
-numMeas = nbrReadoutPulses^nbrQubits;
-
-measMap = zeros(numMeas, numPreps, 'uint8');
-measMat = zeros(numMeas, numPreps, 'double');
-numSeqs = size(data,1);
-expPerSeq = size(data,2)-2^nbrQubits*calRepeats; 
-measOps = cell(numSeqs,1);
-
-idx=1;
-for seqct = 1:size(data,1)
-    cals = data(seqct,end-2^nbrQubits*calRepeats+1:end);
-    raws = data(seqct,1:end-2^nbrQubits*calRepeats);
-    measOps{seqct} = diag(mean(reshape(cals, calRepeats, 2^nbrQubits),1));
-    measMat(idx:idx+expPerSeq-1) = raws;
-    measMap(idx:idx+expPerSeq-1) = seqct;
-    idx = idx+expPerSeq;
+%First cat multi-measurement data together
+if iscell(data)
+    numMeasChans = length(data);
+    data = cat(1, data{:});
+else
+    numMeasChans = 1;
 end
 
+%Number of different preparations and readouts
+numPreps = nbrPrepPulses^nbrQubits;
+numMeas = nbrReadoutPulses^nbrQubits;
+numExps = numPreps*numMeas*numMeasChans;
+numCals = 2^(nbrQubits)*nbrCalRepeats;
+
+%Rough rescaling by the variance to equalize things for the least squares 
+approxScale = std(data(:,end-numCals+1:end), 0, 2);
+data = bsxfun(@rdivide, data, approxScale);
+
+%Pull out the raw experimental data
+rawData = data(:, 1:numMeas);
+
+%Pull out the calibration data as measurement operators and assign each exp. to a meas. operator 
+measOps = cell(size(data,1),1);
+measMap = nan(numExps,1);
+results = nan(numExps,1);
+
+%Go through row first as fast axis
+idx = 1;
+for row = 1:size(rawData,1)
+    measOps{row} = diag(mean(reshape(data(row, end-numCals+1:end), nbrCalRepeats, 2^nbrQubits),1));
+    for col = 1:size(rawData,2)
+        results(idx) = rawData(row,col);
+        measMap(idx) = row;
+        idx = idx + 1;
+    end
+end
 
 %Setup the state preparation and measurement pulse sets
 U_preps = tomo_gate_set(nbrQubits, nbrPrepPulses);
 U_meas  = tomo_gate_set(nbrQubits, nbrReadoutPulses);
 
 %Call the SDP program to do the constrained optimization
-[choiSDP, choiLSQ] = QPT_SDP(measMat, measOps, measMap, U_preps, U_meas, nbrQubits);
+[choiSDP, choiLSQ] = QPT_SDP(results, measOps, measMap, U_preps, U_meas, nbrQubits);
 
 %Calculate the overlaps with the ideal gate
 if ischar(idealProcess)
@@ -59,6 +77,15 @@ gateFidelity = (2^nbrQubits*processFidelity+1)/(2^nbrQubits+1)
 pauliMapIdeal = choi2pauliMap(choiIdeal);
 pauliMapLSQ = choi2pauliMap(choiLSQ);
 pauliMapExp = choi2pauliMap(choiSDP);
+
+%Permute according to hamming weight
+weights = cellfun(@pauliHamming, pauliStrs);
+[~, weightIdx] = sort(weights);
+
+pauliMapIdeal = pauliMapIdeal(weightIdx, weightIdx);
+pauliMapLSQ = pauliMapLSQ(weightIdx, weightIdx);
+pauliMapExp = pauliMapExp(weightIdx, weightIdx);
+pauliStrs = pauliStrs(weightIdx);
 
 %Create red-blue colorscale
 cmap = [hot(50); 1-hot(50)];
