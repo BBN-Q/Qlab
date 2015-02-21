56d08a9a2c1dbd9820da4396e4b2da5ba0fd5102
CoherenceController@Draco <qlab@bbn.com>
2015-02-20 21:06:24 -0500

    analyzeStateTomo updates.
    Make use of the pauliSetPlot() method.
    
    --BRJ

--- analysis/cQED_dataAnalysis/analyzeStateTomo.m
+++ analysis/cQED_dataAnalysis/analyzeStateTomo.m
@@ -42,47 +42,20 @@ varMat = diag(varData);
 %First least squares
 rhoLSQ = QST_LSQ(tomoData, varMat, measPulseMap, measOpMap, measPulseUs, measOps, nbrQubits);
 
-function pauli_label()
-    [~, pauliStrs] = paulis(nbrQubits);
-    set(gca(), 'XTick', 1:4^nbrQubits); 
-    set(gca(), 'XTickLabel', pauliStrs);
-end
-
-figure()
-subplot(2,1,1)
-bar(rho2pauli(rhoLSQ));
-pauli_label();
+pauliSetPlot(rho2pauli(rhoLSQ));
 title('LSQ Inversion Pauli Decomposition');
-
 
 %Now constrained SDP
 rhoSDP = QST_SDP_uncorrelated(tomoData, varMat, measPulseMap, measOpMap, measPulseUs, measOps, nbrQubits);
 
-subplot(2,1,2)
-bar(rho2pauli(rhoSDP));
-pauli_label();
+pauliSetPlot(rho2pauli(rhoSDP));
 title('SDP Inversion Pauli Decomposition');
 
 
 % rhoRaw = getRho(raw_paulis);
-% rhoWizard = WizardTomo_(rhoRaw, nbrQubits);
-% rhoPlot(rhoWizard);
-% purity = real(trace(rhoWizard^2))
-% 
-% pauliLabels = {'II', 'XI', 'YI', 'ZI', 'IX', 'IY', 'IZ', 'XY', 'XZ', 'YX', 'YZ', 'ZX', 'ZY', 'XX', 'YY', 'ZZ'};
-% [pauliOps, pauliStrs] = paulis(nbrQubits);
-% 
-% paulisWiz = zeros(16,1);
-% for ct = 1:length(pauliOps)
-%     pauliNum = find(strcmp(pauliLabels{ct}, pauliStrs));
-%     paulisWiz(ct) = real(trace(rhoWizard*pauliOps{pauliNum}));
-% end
-% figure;
-% bar(paulisWiz)
-% PauliLabel()
-% title('Constrained Tomography Pauli Decomposition','FontSize',14);
-% xlabel('Pauli Operator','FontSize',12);
-% ylabel('Overlap','FontSize',12);
+% rhoWizard = WizardTomo_(rhoLSQ, nbrQubits);
+% pauliSetPlot(rho2pauli(rhoWizard));
+% title('Wizard Pauli Decomposition');
 % 
 % C = Concurrence_(rhoWizard);
 
