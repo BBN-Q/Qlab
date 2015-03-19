8dccb89b734f8b59ffcacfe5243bd2131e7bc113
CoherenceController@Draco <qlab@bbn.com>
2015-03-19 17:03:52 -0400

    Order of pulses (n=4) compatible with QGL
    --CAR and DR

--- analysis/cQED_dataAnalysis/tomo_gate_set.m
+++ analysis/cQED_dataAnalysis/tomo_gate_set.m
@@ -14,9 +14,9 @@ switch nbrPulses
     case 4
         %Four pulse set
         Uset1Q{1}=eye(2);
-        Uset1Q{2}=expm(-1i*(pi/2)*X);
-        Uset1Q{3}=expm(-1i*(pi/4)*X);
-        Uset1Q{4}=expm(-1i*(pi/4)*Y);
+        Uset1Q{2}=expm(-1i*(pi/4)*X);
+        Uset1Q{3}=expm(-1i*(pi/4)*Y);
+        Uset1Q{4}=expm(-1i*(pi/2)*X);
     case 6
         %Six pulse set
         Uset1Q{1}=eye(2);
