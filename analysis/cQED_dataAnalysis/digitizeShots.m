a31bb50da2f93f2691b5f136980903c8bf9bf9ed
CoherenceController@Draco <qlab@bbn.com>
2015-05-07 16:21:38 -0400

    Fixed bugs in digitizeShots for LNP
    --DR

--- analysis/cQED_dataAnalysis/digitizeShots.m
+++ analysis/cQED_dataAnalysis/digitizeShots.m
@@ -4,16 +4,18 @@ function [codemat, resultvec, thrvec] = digitizeShots(data,LPNq,ndataq,doDig,sig
 %segvec_1 = sorted indeces corresponding to calibration for one qubit in
 %|1> and the rest in |0>
 
+ndata = 10; %number of adjacent shots between calibration points
+ncal = 2^(ndataq+1)*2; %number of calibration points every ndata shots
+
 if ndataq==2
-indmeas=[2,3,1]; %A, Q1, Q2
-segvec_1=[103,109,105];
+indmeas=[2,4,3]; %A, Q1, Q5
+segvec_1=ndata+[5,9,3];
+%segvec_1=[103,109,105];
+
 else %ndataq=3
 indmeas=[2,4,1,3]; %A, Q1, Q2, Q5
-segvec_1=[105,117,109,103];
+segvec_1=ndata+[5,17,9,3];
 end
-
-ndata = 100; %number of adjacent shots between calibration points
-ncal = 2^(ndataq+1)*2; %number of calibration points every ndata shots
 
 totshots = size(data.data{1},1)*size(data.data{1},2);
 repeatNum = totshots/(ndata+ncal);
@@ -29,9 +31,10 @@ for kk=1:ndataq+1
     tempdata=data.data{indmeas(kk)};
     tempdata = real(reshape(tempdata',totshots,1));
     datamat(:,kk)=tempdata; %reorder data
-    [bins(kk,:), PDFvec(:,:,kk), ~,avgdata(:,kk),thrvec(kk)] = initByMsmt(tempdata,ndata+ncal,1,[101;segvec_1(kk)],[0],1,0,0);
+    [bins(kk,:), PDFvec(:,:,kk), ~,avgdata(:,kk),thrvec(kk)] = initByMsmt(tempdata,ndata+ncal,1,[ndata+1;segvec_1(kk)],[0],1,0,0);
 end
-thr_mA= thrvec(1); %bins_m3(find(cumsum(PDFvec_m3(:,101))>ancilla_err,1));  %choose the second option for stricter postselection
+%thr_mA= thrvec(1); 
+thr_mA = bins(1,find(cumsum(PDFvec(:,ndata+1,1))>ancilla_err,1));  %choose this option for stricter postselection
 
 codemat = nan(nshots,ndataq);
 km=1;
@@ -50,8 +53,8 @@ if LPNq==1
                 end
             end
         end
-        if mod(k,100)==0
-            km=km+16;
+        if mod(k,ndata)==0
+            km=km+ncal;
         end
         km=km+1;
     end
@@ -64,8 +67,8 @@ else
             codemat(k,q)= -0.5*(signvec(q+1)-1)*(datamat(km,q+1)>=thrvec(q+1)) + 0.5*(signvec(q+1)+1)*(datamat(km,q+1)<thrvec(q+1));
         end
             resultvec(k) = -0.5*(signvec(1)-1)*(datamat(km,1)>=thr_mA) + 0.5*(signvec(1)+1)*(datamat(km,1)<thr_mA);
-            if mod(k,100)==0
-                km=km+16;
+            if mod(k,ndata)==0
+                km=km+ncal;
             end
             km=km+1;
     end
