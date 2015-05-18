676554f5aaf85dc512e41353c89eeb7312e22ce7
CoherenceController@Draco <qlab@bbn.com>
2015-05-18 17:36:26 -0400

    LPN seqs. for 2 or 3 data qubits
    --DR

--- analysis/cQED_dataAnalysis/LPN.m
+++ analysis/cQED_dataAnalysis/LPN.m
@@ -1,4 +1,5 @@
 function successvec = LPN(data, ndataqubits, codeID, LPNq, maxqueries, nrandom)
+%data, ndataqubits, codeID, LPNq, maxqueries, nrandom
 %codeID = 1...2^ndataqubits. ID for the implemented oracle
 %LPNq = 1 if quantum protocols, 0 if classical
 %maxqueries = max. number of queries analyzed
@@ -6,15 +7,16 @@ function successvec = LPN(data, ndataqubits, codeID, LPNq, maxqueries, nrandom)
 
 signvec = ones(ndataqubits+1,1); %A, Q1, Q2... Assumes that M=0 for M>Mthr.  
 doDig = 0; %if 1 (and LPNq=1), single shots are digitized before a majority vote. If 0, shot voltages are first averaged, then the average is digitized. 
+querystep=10;
 
 [codemat, resultvec, thrvec] = digitizeShots(data,LPNq,ndataqubits,doDig,signvec);
 
 nshots = size(codemat,1);
-successvec = zeros(maxqueries,1);
+successvec = zeros(maxqueries/querystep,1);
 xmat = dec2bin(0:2^ndataqubits-1, ndataqubits);
 distance = zeros(2^size(codemat,2),1);
 
-for nvotes=1:maxqueries
+for nvotes=1:querystep:maxqueries
     yvec=zeros(nvotes,1);
 
     for jj=1:nrandom
@@ -32,15 +34,17 @@ for nvotes=1:maxqueries
         if LPNq==1
             if(doDig==0)
                 samplemat = nanmean(samplemat,1);
-                for kk = 1:ndataqubits
-                    samplemat(1,kk) = -0.5*(signvec(1+kk)-1)*(samplemat(1,kk)>=thrvec(1+kk)) + 0.5*(signvec(1+kk)+1)*(samplemat(1,kk)<thrvec(1+kk));
+                if sum(isnan(samplemat))==0
+                    for kk = 1:ndataqubits
+                        samplemat(1,kk) = -0.5*(signvec(1+kk)-1)*(samplemat(1,kk)>=thrvec(1+kk)) + 0.5*(signvec(1+kk)+1)*(samplemat(1,kk)<thrvec(1+kk));
+                    end
                 end
                 majvote = samplemat;
             else
                 majvote = mode(samplemat);
             end
             if majvote == str2num(reshape(xmat(codeID,:)',[],1))'; 
-                successvec(nvotes)=successvec(nvotes)+1;
+                successvec((nvotes-1)/querystep+1)=successvec((nvotes-1)/querystep+1)+1;
             end
         else
             for t=1:size(xmat,1)
@@ -49,7 +53,7 @@ for nvotes=1:maxqueries
             end
             [~,indmin]=min(distance);
             if indmin==codeID 
-                successvec(nvotes)=successvec(nvotes)+1;
+                successvec((nvotes-1)/querystep+1)=successvec((nvotes-1)/querystep+1)+1;
             end
             
         end        
@@ -58,7 +62,8 @@ end
 successvec=successvec/nrandom;
 
 figure(202); 
-plot(successvec); ylim([0,1]); 
+hold on;
+plot(1:querystep:maxqueries,successvec); ylim([0,1]); 
         
     
 
--- experiments/LPN/LPNSequence.py
+++ experiments/LPN/LPNSequence.py
@@ -4,6 +4,7 @@ from copy import copy
 
 parser = argparse.ArgumentParser()
 parser.add_argument('pyqlabpath', help='path to PyQLab directory')
+parser.add_argument('datanum', type=int, help='number of data qubits')
 parser.add_argument('seqtype', help='c/q for classical/quantum')
 parser.add_argument('seqkey', help='implemented oracle')
 parser.add_argument('ancilla', help='ancilla qubit name')
@@ -19,52 +20,78 @@ args = parser.parse_args()
 
 sys.path.append(args.pyqlabpath)
 execfile(os.path.join(args.pyqlabpath, 'startup.py'))
+
+n = args.datanum
+
 a = QubitFactory(args.ancilla)
 d1 = QubitFactory(args.data1)
 d2 = QubitFactory(args.data2)
-d3 = QubitFactory(args.data3)
 CRd1 = QubitFactory(args.CR1)
 CRd2 = QubitFactory(args.CR2)
-CRd3 = QubitFactory(args.CR3)
+if n>2:
+	d3 = QubitFactory(args.data3)
+	CRd3 = QubitFactory(args.CR3)
 
 seqkey = args.seqkey
 seqtype = args.seqtype
 
 seqCRd1 = ZX90_CR(a,d1,CRd1)
 seqCRd2 = ZX90_CR(a,d2,CRd2)
-seqCRd3 = ZX90_CR(a,d3,CRd3)
+if n>2:
+	seqCRd3 = ZX90_CR(a,d3,CRd3)
 
-if seqtype == 'q':
-	Hadgates = [Y90(a)*Y90(d1)*Y90(d2)*Y90(d3), X(a)*X(d1)*X(d2)*X(d3)]
-else:
-	Hadgates = []
+if n==3: #currently largest LPN exp.
+	if seqtype == 'q':
+		Hadgates = [Y90(a)*Y90(d1)*Y90(d2)*Y90(d3), X(a)*X(d1)*X(d2)*X(d3)]
+	else:
+		Hadgates = []
 
-if seqkey == '111':
-	seq = [Y90(a)] + seqCRd2 + [Z90m(a)]+ seqCRd3 + [Z90m(a)] + seqCRd1 + [Z90m(a), Y90(a)*X90(d1)*X90(d2)*X90(d3),X(a)*Y90m(d1)*Y90m(d2)*Y90m(d3)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)] 
-elif seqkey == '110':
-    seq = [Y90(a)*Y90(d3)] + seqCRd2 + [Z90m(a)]+ seqCRd1 + [Z90m(a), Y90(a)*X90(d1)*X90(d2),X(a)*Y90m(d1)*Y90m(d2)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)] 
-elif seqkey == '101':
-	seq = [Y90(a)*Y90(d2)] + seqCRd3 + [Z90m(a)]+ seqCRd1 + [Z90m(a), Y90(a)*X90(d1)*X90(d3),X(a)*Y90m(d1)*Y90m(d3)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)] 
-elif seqkey == '100':
-	seq = [Y90(a)*Y90(d2)*Y90(d3)] + seqCRd1 + [Z90m(a), Y90(a)*X90(d1),X(a)*Y90m(d1)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)] 
-elif seqkey == '011':
-	seq = [Y90(a)*Y90(d1)] + seqCRd2 + [Z90m(a)]+ seqCRd3 + [Z90m(a)] + [Y90(a)*X90(d2)*X90(d3),X(a)*Y90m(d2)*Y90m(d3)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)] 
-elif seqkey == '010':
-	seq = [Id(CRd1), Y90(a)*Y90(d1)*Y90(d3)] + seqCRd2 + [Z90m(a)] + [Y90(a)*X90(d2),X(a)*Y90m(d2)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)] #the Id on CRd1 is necessary to overwrite the seq. in the APS, with the current config. 
-elif seqkey == '001':
-	seq = [Y90(a)*Y90(d1)*Y90(d2)] + seqCRd3 + [Z90m(a)]+  [Y90(a)*X90(d3),X(a)*Y90m(d3)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)] 
-elif seqkey == '000':
-	seq = [Id(CRd1), Id(a)*Y90(d1)*Y90(d2)*Y90(d3)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)]
+	if seqkey == '111':
+		seq = [Y90(a)] + seqCRd2 + [Z90m(a)]+ seqCRd3 + [Z90m(a)] + seqCRd1 + [Z90m(a), Y90(a)*X90(d1)*X90(d2)*X90(d3),X(a)*Y90m(d1)*Y90m(d2)*Y90m(d3)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)] 
+	elif seqkey == '110':
+	    seq = [Id(CRd3), Y90(a)*Y90(d3)] + seqCRd2 + [Z90m(a)]+ seqCRd1 + [Z90m(a), Y90(a)*X90(d1)*X90(d2),X(a)*Y90m(d1)*Y90m(d2)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)] 
+	elif seqkey == '101':
+		seq = [Id(CRd2), Y90(a)*Y90(d2)] + seqCRd3 + [Z90m(a)]+ seqCRd1 + [Z90m(a), Y90(a)*X90(d1)*X90(d3),X(a)*Y90m(d1)*Y90m(d3)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)] 
+	elif seqkey == '100':
+		seq = [Id(CRd2)*Id(CRd3), Y90(a)*Y90(d2)*Y90(d3)] + seqCRd1 + [Z90m(a), Y90(a)*X90(d1),X(a)*Y90m(d1)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)] 
+	elif seqkey == '011':
+		seq = [Id(CRd1), Y90(a)*Y90(d1)] + seqCRd2 + [Z90m(a)]+ seqCRd3 + [Z90m(a)] + [Y90(a)*X90(d2)*X90(d3),X(a)*Y90m(d2)*Y90m(d3)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)] 
+	elif seqkey == '010':
+		seq = [Id(CRd1)*Id(CRd3), Y90(a)*Y90(d1)*Y90(d3)] + seqCRd2 + [Z90m(a)] + [Y90(a)*X90(d2),X(a)*Y90m(d2)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)] #the Id on CRd1 is necessary to overwrite the seq. in the APS, with the current config. 
+	elif seqkey == '001':
+		seq = [Id(CRd1)*Id(CRd2), Y90(a)*Y90(d1)*Y90(d2)] + seqCRd3 + [Z90m(a)]+  [Y90(a)*X90(d3),X(a)*Y90m(d3)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)] 
+	elif seqkey == '000':
+		seq = [Id(CRd1)*Id(CRd2)*Id(CRd3), Id(a)*Y90(d1)*Y90(d2)*Y90(d3)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)]
+elif n==2: #reduced LPN
+	if seqtype == 'q':
+		Hadgates = [Y90(a)*Y90(d1)*Y90(d2), X(a)*X(d1)*X(d2)]
+	else:
+		Hadgates = []
+
+	if seqkey == '11':
+		seq = [Y90(a)] + seqCRd2 + [Z90m(a)] + seqCRd1 + [Z90m(a), Y90(a)*X90(d1)*X90(d2),X(a)*Y90m(d1)*Y90m(d2)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)] 
+	elif seqkey == '10':
+		seq = [Id(CRd2), Y90(a)*Y90(d2)] + seqCRd1 + [Z90m(a), Y90(a)*X90(d1),X(a)*Y90m(d1)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)] 
+	elif seqkey == '01':
+		seq = [Id(CRd1), Y90(a)*Y90(d1)] + seqCRd2 + [Z90m(a), Y90(a)*X90(d2),X(a)*Y90m(d2)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)] 
+	elif seqkey == '00':
+		seq = [Id(CRd1)*Id(CRd2), Id(a)*Y90(d1)*Y90(d2)] + Hadgates + [MEAS(a)*MEAS(d1)*MEAS(d2)] 
 
 seqs = []
 for _ in range(10):
 	seqs+=[copy(seq)]
 
 #add calibration points
-seqs+=[[MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)]]
-seqs+=[[X(d3), MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)]]
-seqs+=[[X(a), MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)]]
-seqs+=[[X(d2), MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)]]
-seqs+=[[X(d1), MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)]]
+if n==3:
+	seqs+=[[MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)]]
+	seqs+=[[X(d3), MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)]]
+	seqs+=[[X(a), MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)]]
+	seqs+=[[X(d2), MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)]]
+	seqs+=[[X(d1), MEAS(a)*MEAS(d1)*MEAS(d2)*MEAS(d3)]]
+elif n==2:
+	seqs+=[[MEAS(a)*MEAS(d1)*MEAS(d2)]]
+	seqs+=[[X(d2), MEAS(a)*MEAS(d1)*MEAS(d2)]]
+	seqs+=[[X(a), MEAS(a)*MEAS(d1)*MEAS(d2)]]
+	seqs+=[[X(d1), MEAS(a)*MEAS(d1)*MEAS(d2)]]
 
 fileNames = compile_to_hardware(seqs, 'LPN/LPN')
