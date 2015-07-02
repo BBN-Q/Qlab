c4f98a855f6c627e6b577d751b866121120d9e41
CoherenceController@Draco <qlab@bbn.com>
2015-07-02 14:24:52 -0400

    LPN sequences with up to 5 qubits
    --DR

--- experiments/LPN/LPNSequence.m
+++ experiments/LPN/LPNSequence.m
@@ -1,7 +1,7 @@
-function LPNSequence(ndataq, seqtype, seqkey, ancilla, data1, data2, data3, CR1, CR2, CR3)
+function LPNSequence(ndataq, seqtype, seqkey, ancilla, data1, data2, data3, data4, CR1, CR2, CR3)
 
 [thisPath, ~] = fileparts(mfilename('fullpath'));
 scriptName = fullfile(thisPath, 'LPNSequence.py');
-[status, result] = system(sprintf('python "%s" "%s" %d %s %s %s %s %s %s %s %s %s', scriptName, getpref('qlab', 'PyQLabDir'), ndataq, seqtype, seqkey, ancilla, data1, data2, data3, CR1, CR2, CR3), '-echo');
+[status, result] = system(sprintf('python "%s" "%s" %d %s %s %s %s %s %s %s %s %s %s', scriptName, getpref('qlab', 'PyQLabDir'), ndataq, seqtype, seqkey, ancilla, data1, data2, data3, data4, CR1, CR2, CR3), '-echo');
 
 end
