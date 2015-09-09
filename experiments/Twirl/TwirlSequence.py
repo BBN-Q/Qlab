f611beaf08a49bb74aa66212ee6d67837821ce28
CoherenceController@Draco <qlab@bbn.com>
2015-09-09 16:53:07 -0400

    Generates Twirl seq via Matlab
    --DR

--- experiments/Twirl/TwirlSequence.py
+++ experiments/Twirl/TwirlSequence.py
@@ -21,5 +21,12 @@ CR = QubitFactory('CR')
 CR2 = QubitFactory('CR2')
 CR5 = QubitFactory('CR5')
 
-seq = twirl_seq_4q(q1, q2, q3, q5, [Id(q1)*Id(q2)*Id(q3)*Id(q5)*Id(CR5)*Id(CR2)*Id(CR)], "C:/Users/qlab/Documents/Julia/Twirl/twirl_IIIs%d.csv" %args.seq) + create_cal_seqs((q1,q2,q3,q5), 1)
+#seq = twirl_seq_4q(q1, q2, q3, q5, [Id(q1)*Id(q2)*Id(q3)*Y90(q5)*Id(CR5)*Id(CR2)*Id(CR)], "C:/Users/qlab/Documents/Julia/Twirl/twirl_IIIY90s%d.csv" %args.seq) + create_cal_seqs((q1,q2,q3,q5), 1)
+seqCRd1 = ZX90_CR(q3,q1,CR)
+seqCRd2 = ZX90_CR(q3,q2,CR2)
+seqCRd3 = ZX90_CR(q3,q5,CR5)
+
+seqLPN = [Y90(q3)] + seqCRd2 + [Z90m(q3)]+ seqCRd3 + [Z90m(q3)] + seqCRd1 + [Z90m(q3), Y90(q3)*X90(q1)*X90(q2)*X90(q5),X(q3)*Y90m(q1)*Y90m(q2)*Y90m(q5)] + [Y90(q3)*Y90(q1)*Y90(q2)*Y90(q5), X(q3)*X(q1)*X(q2)*X(q5)] 
+seq = twirl_seq_4q(q1, q2, q3, q5, seqLPN, "C:/Users/qlab/Documents/Julia/Twirl/twirl_LPN%d.csv" %args.seq) + create_cal_seqs((q1,q2,q3,q5), 1)
+
 fileNames = compile_to_hardware(seq, "Twirl/Twirl")
