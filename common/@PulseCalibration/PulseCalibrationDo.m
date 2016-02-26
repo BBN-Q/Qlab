84514e1f5be30c63e48b22af0ef793a25506908e
CoherenceController <qlab@bbn.com>
2016-02-26 09:23:05 -0500

    Warning when calibrated amp. > 1
    --DR

--- common/@PulseCalibration/PulseCalibrationDo.m
+++ common/@PulseCalibration/PulseCalibrationDo.m
@@ -132,6 +132,9 @@ if settings.DoPi2Cal
     i_offset = real(x0(2));
     obj.channelParams.i_offset = i_offset;
     fprintf('Found X90Amp: %.4f\n', X90Amp);
+    if X90Amp>1
+        warning('X90Amp over range!');
+    end
     fprintf('Found I offset: %.4f\n\n\n', i_offset);
     
     % calibrate amplitude and offset for +/- Y90
@@ -142,6 +145,9 @@ if settings.DoPi2Cal
         Y90Amp = real(x0(1));
         q_offset = real(x0(2));
         fprintf('Found Y90Amp: %.4f\n', Y90Amp);
+        if X90Amp>1
+            warning('Y90Amp over range!');
+        end
         fprintf('Found Q offset: %.4f\n\n\n', q_offset);
         
         % update channelParams
@@ -162,11 +168,17 @@ if settings.DoPi2PhaseCal
     X90Amp = obj.optimize_amplitude(obj.channelParams.pi2Amp, 'X', pi/2);
     obj.channelParams.pi2Amp = X90Amp;
     fprintf('Found X90Amp: %.4f\n\n', X90Amp);
+    if X90Amp>1
+        warning('X90Amp over range!');
+    end
 
     % calibrate Y90 if not using SSB
     if obj.channelParams.SSBFreq == 0
         Y90Amp = obj.optimize_amplitude(X90Amp, 'Y', pi/2);
         fprintf('Found Y90Amp: %.4f\n', Y90Amp);
+        if Y90Amp>1
+            warning('Y90Amp over range!');
+        end
 
         obj.channelParams.pi2Amp = Y90Amp;
         % update T matrix with ratio X90Amp/Y90Amp
@@ -188,7 +200,10 @@ if settings.DoPiCal
     X180Amp = real(x0(1));
     i_offset = real(x0(2));
     fprintf('Found X180Amp: %.4f\n\n\n', X180Amp);
-    
+    if X180Amp>1
+        warning('X180Amp over range!');
+    end
+
     % update channelParams
     obj.channelParams.piAmp = X180Amp;
     obj.channelParams.i_offset = i_offset;
@@ -201,6 +216,10 @@ if settings.DoPiPhaseCal
     X180Amp = obj.optimize_amplitude(obj.channelParams.piAmp, 'X', pi);
     obj.channelParams.piAmp = X180Amp;
     fprintf('Found X180Amp: %.4f\n\n', X180Amp);
+    if X180Amp>1
+        warning('X180Amp over range!');
+    end
+
     updateQubitPulseParams(obj.settings.Qubit, obj.channelParams);
 end
 
