0e7bc9ee8060992ed67a1d56819321ef10165039
CoherenceController <qlab@bbn.com>
2016-03-29 10:57:32 -0400

    Move common expSettings out of the switch
    --DR

--- experiments/muWaveDetection/calibrateCR.m
+++ experiments/muWaveDetection/calibrateCR.m
@@ -12,7 +12,6 @@ parse(p, varargin{:});
 expSettings = p.Results.expSettings;
 calSteps = p.Results.calSteps;
 
-optlen=NaN;
 optphase = NaN;
 contrast = NaN;
 optamp = NaN;
@@ -60,25 +59,25 @@ for measname = fieldnames(expSettings.measurements)'
     end
 end
 
+expSettings.sweeps={};
+expSettings.sweeps.SegmentNumWithCals={};
+expSettings.sweeps.SegmentNumWithCals.order=1;
+expSettings.sweeps.SegmentNumWithCals.type='SegmentNum';
+expSettings.sweeps.SegmentNumWithCals.numPoints = 46;
+expSettings.sweeps.SegmentNumWithCals.numCals = 8;
+
 if calSteps(1)
     %create a sequence with desired range of CR pulse lengths
     CRCalSequence(CalParams.control, CalParams.target, 1, CalParams.lenstep)
     
     %updates sweep settings
-    expSettings.sweeps={};
-    expSettings.sweeps.SegmentNumWithCals={};
-    expSettings.sweeps.SegmentNumWithCals.order=1;
-    expSettings.sweeps.SegmentNumWithCals.type='SegmentNum';
     expSettings.sweeps.SegmentNumWithCals.start = CalParams.lenstep;
     expSettings.sweeps.SegmentNumWithCals.stop = CalParams.lenstep*38;
-    expSettings.sweeps.SegmentNumWithCals.numPoints = 46;
-    expSettings.sweeps.SegmentNumWithCals.numCals = 8;
     expSettings.sweeps.SegmentNumWithCals.step = (expSettings.sweeps.SegmentNumWithCals.stop-expSettings.sweeps.SegmentNumWithCals.start)/(expSettings.sweeps.SegmentNumWithCals.numPoints-expSettings.sweeps.SegmentNumWithCals.numCals-1);
     expSettings.sweeps.SegmentNumWithCals.axisLabel = 'Pulse top length (ns)';
     
     expSettings.instruments = instrSettings;
     expSettings.saveAllSettings = false;
-    pause(0.5);
     
     %run the sequence
     ExpScripter2('CRcal_len', expSettings, 'lockSegments');
@@ -91,45 +90,36 @@ else
 end
 
 if calSteps(2)
-%create a sequence with calibrated length and variable phase
-
-CRCalSequence(CalParams.control, CalParams.target, 2, optlen) 
-
-%updates sweep settings
-expSettings.sweeps.SegmentNumWithCals.start = 0;
-expSettings.sweeps.SegmentNumWithCals.stop = 720;
-expSettings.sweeps.SegmentNumWithCals.numPoints = 46;
-expSettings.sweeps.SegmentNumWithCals.numCals = 8;
-expSettings.sweeps.SegmentNumWithCals.step = (expSettings.sweeps.SegmentNumWithCals.stop-expSettings.sweeps.SegmentNumWithCals.start)/(expSettings.sweeps.SegmentNumWithCals.numPoints-expSettings.sweeps.SegmentNumWithCals.numCals-1);
-expSettings.sweeps.SegmentNumWithCals.axisLabel = 'Pulse phase (deg)';
-
-%run the sequence
-ExpScripter2('CRcal_ph', expSettings, 'lockSegments');
-
-%analyze the sequence and updates CR pulse
-data=load_data('latest');
-[optphase, contrast] = analyzeCalCR(2,data,CalParams.channel,CalParams.CR);
+    %create a sequence with calibrated length and variable phase
+    
+    CRCalSequence(CalParams.control, CalParams.target, 2, optlen)
+    
+    %updates sweep settings
+    expSettings.sweeps.SegmentNumWithCals.start = 0;
+    expSettings.sweeps.SegmentNumWithCals.stop = 720;
+    expSettings.sweeps.SegmentNumWithCals.step = (expSettings.sweeps.SegmentNumWithCals.stop-expSettings.sweeps.SegmentNumWithCals.start)/(expSettings.sweeps.SegmentNumWithCals.numPoints-expSettings.sweeps.SegmentNumWithCals.numCals-1);
+    expSettings.sweeps.SegmentNumWithCals.axisLabel = 'Pulse phase (deg)';
+    
+    %run the sequence
+    ExpScripter2('CRcal_ph', expSettings, 'lockSegments');
+    
+    %analyze the sequence and updates CR pulse
+    data=load_data('latest');
+    [optphase, contrast] = analyzeCalCR(2,data,CalParams.channel,CalParams.CR);
 end
 
 if calSteps(3)
     %create a sequence with desired range of CR pulse amplitudes
     CRCalSequence(CalParams.control, CalParams.target, 3, optlen)
     
-    %updates sweep settings
-    expSettings.sweeps={};
-    expSettings.sweeps.SegmentNumWithCals={};
-    expSettings.sweeps.SegmentNumWithCals.order=1;
-    expSettings.sweeps.SegmentNumWithCals.type='SegmentNum';
+    %updates sweep settings;
     expSettings.sweeps.SegmentNumWithCals.start = 0.6;
     expSettings.sweeps.SegmentNumWithCals.stop = 1.4;
-    expSettings.sweeps.SegmentNumWithCals.numPoints = 46;
-    expSettings.sweeps.SegmentNumWithCals.numCals = 8;
     expSettings.sweeps.SegmentNumWithCals.step = (expSettings.sweeps.SegmentNumWithCals.stop-expSettings.sweeps.SegmentNumWithCals.start)/(expSettings.sweeps.SegmentNumWithCals.numPoints-expSettings.sweeps.SegmentNumWithCals.numCals-1);
-    expSettings.sweeps.SegmentNumWithCals.axisLabel = 'Pulse ampliude (V)';
+    expSettings.sweeps.SegmentNumWithCals.axisLabel = 'Pulse amplitude (V)';
     
     expSettings.instruments = instrSettings;
     expSettings.saveAllSettings = false;
-    pause(0.5);
     
     %run the sequence
     ExpScripter2('CRcal_amp', expSettings, 'lockSegments');
