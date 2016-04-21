70bef28c4fe99e8590cd830936411ec88a516cac
Colm Ryan <cryan@bbn.com>
2016-04-21 09:17:07 -0400

    :construction: work towards using APS2 built in mixer correction

--- experiments/muWaveDetection/src/@MixerOptimizer/MixerOptimizer.m
+++ experiments/muWaveDetection/src/@MixerOptimizer/MixerOptimizer.m
@@ -37,15 +37,15 @@ classdef MixerOptimizer < handle
         results = struct('iOffset',0, 'qOffset',0, 'ampFactor', 1, 'phaseSkew', 0)
         prompt %if 1, user input is required to accept the calibration result
     end
-    
+
     methods
         % constructor
         function obj = MixerOptimizer()
         end
-        
+
         %% class methods
         function Init(obj, cfgFile, chan, prompt)
-            
+
             % pull in channel parameters from requested logical channel in the Qubit2ChannelMap
             %Quiet down warnings from '-''s in fieldnames
             warning('off', 'json:fieldNameConflict');
@@ -54,12 +54,12 @@ classdef MixerOptimizer < handle
             assert(isfield(channelLib.channelDict, genvarname(chan)), 'Qubit %s not found in channel library', chan);
             obj.chan = chan;
             obj.channelParams = channelLib.channelDict.(genvarname(chan));
-            
+
             % load any optimize mixer specific configurations
             settings = json.read(cfgFile);
             obj.expParams = settings.ExpParams;
             instrSettings = settings.InstrParams;
-            
+
             %Get references to the AWG, uW source, and spectrum analyzer
             tmpStr = regexp(obj.channelParams.physChan, '-', 'split'); % split on '-'
             awgName = tmpStr{1};
@@ -69,31 +69,31 @@ classdef MixerOptimizer < handle
             % not compatible with awg.setAll()
             instrLib = json.read(getpref('qlab', 'CurScripterFile'));
             obj.awg.setAll(instrLib.instruments.(awgName));
-            
+
             %pass physChan through genvarname to get a MATLAB compatible name
             mangledPhysChan = genvarname(obj.channelParams.physChan);
             sourceName = channelLib.channelDict.(mangledPhysChan).generator;
             obj.uwsource = InstrumentFactory(sourceName);
             obj.uwsource.setAll(instrLib.instruments.(sourceName)); %catch any frequency updates from GUI
             obj.sa = InstrumentFactory(obj.expParams.specAnalyzer);
-            
+
             obj.sa.setAll(instrSettings.(obj.expParams.specAnalyzer));
             obj.sa.centerFreq = obj.uwsource.frequency;
-            
+
             %Turn the uwave source on and turn modulation off
             obj.uwsource.mod = 0;
             obj.uwsource.pulse = 0;
             obj.uwsource.output = 1;
-            
+
             obj.prompt = prompt;
         end
-        
+
         function Do(obj)
-            
+
             %Set the cleanup function so that even if we ctrl-c out we
             %correctly cleanup
             c = onCleanup(@() obj.cleanUp());
-            
+
             switch obj.optimMode
                 case 'sweep'
                     obj.setup_SSB_AWG(0,0);
@@ -108,10 +108,10 @@ classdef MixerOptimizer < handle
                 otherwise
                     error('Unknown optimMode');
             end
-            
-            
+
+
         end
-        
+
         function stop = LMStoppingCondition(obj, ~, optimValues, ~)
             if 10*log10(optimValues.resnorm) < obj.costFunctionGoal
                 stop = true;
@@ -119,9 +119,9 @@ classdef MixerOptimizer < handle
                 stop = false;
             end
         end
-        
+
         function cleanUp(obj)
-            
+
             % restore instruments to a normal state
             obj.sa.centerFreq = obj.uwsource.frequency;
             obj.sa.span = obj.expParams.SSBFreq * 2.2;
@@ -132,22 +132,22 @@ classdef MixerOptimizer < handle
             obj.sa.video_averaging = 1;
             obj.sa.sweep();
             obj.sa.peakAmplitude();
-            
+
             if obj.prompt
             %Ask whether to write to file or not
                 happy = questdlg('Are you happy with the result?','Optimize Mixer Wrap-up');
             else
                 happy = 'Yes';
             end
-                
+
             switch happy
                 case 'Yes'
-            
+
                 % update transformation matrix params in channel library
                 updateAmpPhase(obj.channelParams.physChan, obj.results.ampFactor, obj.results.phaseSkew);
 
                 % update i and q offsets in the instrument library
-                
+
                 instrLib = json.read(getpref('qlab', 'InstrumentLibraryFile'));
                 tmpStr = regexp(obj.channelParams.physChan, '-', 'split');
                 awgName = tmpStr{1};
@@ -164,18 +164,18 @@ classdef MixerOptimizer < handle
                 case {'No','Cancel'}
                     fprintf('Not writing to file...');
             end
-            
+
             %Stop the AWG
             obj.awg.stop()
-                    
+
             %Print out a summary for the notebook
             fprintf('\nSummary:\n');
             fprintf('i_offset = %.4f; q_offset = %.4f; ampFactor = %.4f; phaseSkew = %.1f\n', ...
                 obj.results.iOffset, obj.results.qOffset, obj.results.ampFactor, obj.results.phaseSkew)
-            
+
         end
-        
-        
+
+
         function setup_SSB_AWG(obj, i_offset, q_offset)
             %Helper function to setup the SSB waveforms from the AWGs
             awgfile = obj.expParams.SSBAWGFile;
@@ -191,13 +191,13 @@ classdef MixerOptimizer < handle
                     obj.awg.setOffset(iChan, i_offset);
                     obj.awg.setSkew(iChan, 0);
                     obj.awg.setEnabled(iChan, 1);
-                    
+
                     obj.awg.setAmplitude(qChan, awg_amp);
                     obj.awg.setOffset(qChan, q_offset);
                     obj.awg.setSkew(qChan, 0);
                     obj.awg.setEnabled(qChan, 1);
                     obj.awg.run();
-                    
+
                 case {'deviceDrivers.APS', 'APS'}
                     %Here we use waveform mode to put out a continuous
                     %sine wave
@@ -207,77 +207,67 @@ classdef MixerOptimizer < handle
                     %channels
                     waveformLength = 1200;
                     tpts = (1/obj.awg.samplingRate)*(0:(waveformLength-1));
-                    
+
                     % i waveform
                     iwf = 0.5 * cos(2*pi*obj.expParams.SSBFreq*tpts);
                     obj.awg.setAmplitude(iChan, awg_amp);
                     obj.awg.setOffset(iChan, i_offset);
                     % waveforms in the range (-1, 1)
                     obj.awg.loadWaveform(iChan, iwf);
-                    
+
                     % q waveform
                     qwf = -0.5 * sin(2*pi*obj.expParams.SSBFreq*tpts);
                     obj.awg.setAmplitude(qChan, awg_amp);
                     obj.awg.setOffset(qChan, q_offset);
                     obj.awg.loadWaveform(qChan, qwf);
-                    
+
                     obj.awg.triggerSource = 'internal';
-                    
+
                     %Set all channels to continuous waveform to avoid a
                     %conflict between the two FPGAs
                     for ct = 1:4
                         obj.awg.setRepeatMode(ct, obj.awg.CONTINUOUS);
                         obj.awg.setRunMode(ct, obj.awg.RUN_WAVEFORM);
                     end
-                    
+
                     %Turn off the unused channels
                     channelsOn = false(1,4);
                     channelsOn([iChan, qChan]) = true;
                     for ct = 1:4
                         obj.awg.setEnabled(ct, channelsOn(ct));
                     end
-                    
+
                     %Get the AWG going
                     obj.awg.run();
                 case 'APS2'
+
+                    awg_amp = get_channel_scale(obj.awg, iChan)
+
+                    %Setup a SSB waveform with a 1200 pt (1us) sinusoid for both
+                    %channels. Scale by 0.5 to give room for sweeps
                     obj.awg.stop();
-                    awg_amp = obj.awg.get_channel_scale(iChan);
-                    
-                    %Setup a SSB waveform with a 1200 pt sinusoid for both
-                    %channels
-                    waveformLength = 1200;
-                    tpts = (1/obj.awg.samplingRate)*(0:(waveformLength-1));
-                    
-                    % i waveform
-                    iwf = 0.5 * cos(2*pi*obj.expParams.SSBFreq*tpts);
-                    obj.awg.set_channel_scale(iChan, awg_amp);
-                    obj.awg.set_channel_offset(iChan, i_offset);
-                    % waveforms in the range (-1, 1)
-                    obj.awg.load_waveform(iChan, iwf);
-                    
-                    % q waveform
-                    qwf = -0.5 * sin(2*pi*obj.expParams.SSBFreq*tpts);
-                    obj.awg.set_channel_scale(qChan, awg_amp);
-                    obj.awg.set_channel_offset(qChan, q_offset);
-                    obj.awg.load_waveform(qChan, qwf);
-                    
-                    obj.awg.set_trigger_source('internal');
-                    obj.awg.set_run_mode('CW_WAVEFORM');
-                    
+                    load_waveform(obj.awg, iChan, 0.5*ones(1200));
+                    load_waveform(obj.awg, qChan, zeros(1200));
+                    set_waveform_frequency(obj.awg, obj.expParams.SSBFreq);
                     for ct = 1:2
-                        obj.awg.set_channel_enabled(ct, true);
+                      obj.awg.set_channel_enabled(ct, true);
                     end
-                    
+                    obj.awg.set_run_mode('CW_WAVEFORM');
+
+                    %Reset current mixer correction
+                    set_mixer_amplitude_imbalance(obj.awg, 1)
+                    set_mixer_phase_skew(obj.awg, 0)
+
                     %Get the AWG going
                     obj.awg.run();
-                    
+
             end
             obj.awgAmp = awg_amp;
         end
-        
-        
+
+
     end
-    
+
     methods(Static)
         %Forward reference a static helper that fits the typical sweep
         %curves
