07de4faa29d7c5150a226d062c81305b50a07a92
CoherenceController <qlab@bbn.com>
2016-05-11 15:29:06 -0400

    Fix uncertainty estimate sigma
    I think this should unwrap the phase as above, so that pi and -pi are
    the same angle
    --DR

--- common/@PulseCalibration/PhaseEstimation.m
+++ common/@PulseCalibration/PhaseEstimation.m
@@ -64,7 +64,7 @@ function [phase, sigma] = PhaseEstimation(data, vardata, verbose)
         end
     end
     phase = curGuess;
-    sigma = max(abs(curGuess - lowerBound), abs(curGuess - upperBound));
+    sigma = max(abs(mod(curGuess - lowerBound + pi, 2*pi) - pi), abs(mod(curGuess - upperBound + pi, 2*pi) - pi));
 end
 
 function out = restrict(phase)
