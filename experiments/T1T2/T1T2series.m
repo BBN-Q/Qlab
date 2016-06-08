23a6f845d652f9f78c12387cd1491282c8c48af9
dieris <driste@bbn.com>
2016-06-08 13:46:16 -0400

    Simple script to loop T1-T2 on multiple qubits
    Support multiple acquisition cards and APS units (similar to pulse
    calibration)

--- experiments/T1T2/T1T2series.m
+++ experiments/T1T2/T1T2series.m
@@ -1,8 +1,30 @@
-function T1T2series(qubitlist, numRepeats)
-%running T1, T2 sequences on multiple qubits
-%qubitlist = {'q1', 'q2', ...}
+function T1T2series(qubitlist, nloops)
+%running T1, T2 sequences for different qubits in a loop
+% qubitlist, e.g. {'q1','q3','q5'};
+% nloops: number of repeats
 
-for k=1:numRepeats
+doT1 = true;
+doRamsey = true;
+doSeqs = true; %generate the seqs
+
+T1Stop = 200e3; T1Step = 2000;
+RamseyStop = 80e3; RamseyStep = 800;
+
+if doSeqs
+    [thisPath, ~] = fileparts(mfilename('fullpath'));
+    for q = 1:length(qubitlist)
+        if doT1
+            scriptName = fullfile(thisPath, 'T1Sequence.py');
+            system(sprintf('python "%s" "%s" %s %d %d', scriptName, getpref('qlab', 'PyQLabDir'), qubitlist{q}, T1Stop, T1Step), '-echo');
+        end
+        if doRamsey
+            scriptName = fullfile(thisPath, 'RamseySequence.py');
+            system(sprintf('python "%s" "%s" %s %d %d', scriptName, getpref('qlab', 'PyQLabDir'), qubitlist{q}, RamseyStop, RamseyStep), '-echo');
+        end
+    end
+end
+
+for k = 1:nloops
 
     for q = 1:length(qubitlist)
         warning('off', 'json:fieldNameConflict');
@@ -13,33 +35,54 @@ for k=1:numRepeats
         
         chanSettings = chanSettings.channelDict;
         
-        expSettings.AWGs = {'BBNAPS2'}; %master AWG
+        expSettings.AWGs = {'APSII4'}; %master AWG
         
         qubit = qubitlist{q}
-        %selects the relevant AWGs
+        %select the relevant AWGs
         tmpStr = regexp(chanSettings.(qubit).physChan, '-', 'split');
         expSettings.AWGs{2} = tmpStr{1};
         tmpStr = regexp(chanSettings.(strcat(genvarname('M-'),qubit)).physChan, '-', 'split');
         expSettings.AWGs{3} = tmpStr{1};
         expSettings.AWGs = unique(expSettings.AWGs); %remove duplicates
-
-        expSettings.AWGfilename = strcat('T1_', qubit);
-        expSettings.sweeps.SegmentNumWithCals.start = 0;
-        expSettings.sweeps.SegmentNumWithCals.stop = 100000;
-        expSettings.sweeps.SegmentNumWithCals.step = 1000;
-        for measname = fieldnames(expSettings.measurements)'
-            if sum(strfind(measname{1},strcat('M',qubit(2))))==0 && ~isempty(strfind(measname{1},'M'))
-                expSettings.measurements = rmfield(expSettings.measurements, measname{1});
+        
+        %add the relevant meas filters and sources
+        measSettings = {};
+        mstr = strcat('M',qubit(2),'Kernel');
+        measSettings.(mstr) = expSettings.measurements.(mstr);
+        dataSource = expSettings.measurements.(mstr).dataSource;
+        for m=1:3
+            %recursively add sources (measurements or instruments)
+            if (~isempty(strfind(dataSource, 'X6')) || ~isempty(strfind(dataSource, 'ATS'))) 
+                for instrName = fieldnames(expSettings.instruments)'
+                    if ((~isempty(strfind(instrName{1}, 'X6')) || ~isempty(strfind(instrName{1}, 'ATS'))) && ~strcmp(dataSource, instrName{1}))
+                        expSettings.instruments = rmfield(expSettings.instruments, instrName{1});
+                    end
+                end
+                break
+            elseif isfield(expSettings.measurements.(dataSource), 'dataSource')
+                measSettings.(dataSource) = expSettings.measurements.(dataSource);
+                dataSource = expSettings.measurements.(dataSource).dataSource;
+            else
+                break
             end
         end
-        ExpScripter2(strcat('T1_',qubit), 'lockSegments', expSettings);
+        expSettings.measurements = measSettings;
         
-        expSettings.sweeps.SegmentNumWithCals.start = 0;
-        expSettings.sweeps.SegmentNumWithCals.stop = 80000;
-        expSettings.sweeps.SegmentNumWithCals.step = 800;
-        expSettings.AWGfilename = strcat('Ramsey_', qubit);
-        ExpScripter2(strcat('Ramsey_',qubit), 'lockSegments', expSettings);
+        if doT1
+            expSettings.AWGfilename = strcat('T1_', qubit);
+            expSettings.sweeps.SegmentNumWithCals.start = 0;
+            expSettings.sweeps.SegmentNumWithCals.stop = T1Stop;
+            expSettings.sweeps.SegmentNumWithCals.step = T1Step;
+            ExpScripter2(strcat('T1_',qubit), 'lockSegments', expSettings);
+        end
         
+        if doRamsey
+            expSettings.AWGfilename = strcat('Ramsey_', qubit);
+            expSettings.sweeps.SegmentNumWithCals.start = 0;
+            expSettings.sweeps.SegmentNumWithCals.stop = RamseyStop;
+            expSettings.sweeps.SegmentNumWithCals.step = RamseyStep;
+            ExpScripter2(strcat('Ramsey_',qubit), 'lockSegments', expSettings);
+        end
     end
 
 end
