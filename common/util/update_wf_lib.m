675fddd696f858a55ea22b3fe342404b180c3377
Colm Ryan <cryan@bbn.com>
2016-06-14 10:40:47 -0400

    :art: PEP8 style function names
    with DR

--- common/util/update_wf_lib.m
+++ common/util/update_wf_lib.m
@@ -1,7 +1,7 @@
-function UpdateWfLib(seqPath, seqName)
+function update_wf_lib(seqPath, seqName)
 
 [thisPath, ~] = fileparts(mfilename('fullpath'));
-scriptName = fullfile(thisPath, 'UpdateWfLib.py');
+scriptName = fullfile(thisPath, 'update_wf_lib.py');
 [status, result] = system(sprintf('python "%s" "%s" %s %s', scriptName, getpref('qlab', 'PyQLabDir'), seqPath, seqName), '-echo');
 
 end
