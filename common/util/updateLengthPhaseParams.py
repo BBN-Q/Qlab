52df830527b4a6c9a38dad11d1ecc5f7a980edfc
dieris <driste@bbn.com>
2016-08-11 09:55:06 -0400

    Remove debugging leftovers

--- common/util/updateLengthPhaseParams.py
+++ common/util/updateLengthPhaseParams.py
@@ -13,11 +13,9 @@ from QGL.ChannelLibrary import channelLib
 
 if args.qubit not in channelLib.channelDict:
 	sys.exit(1)
-	print "here"
 
 channelLib[args.qubit].pulseParams['length'] = args.length
 channelLib[args.qubit].pulseParams['phase'] = args.phase
 channelLib[args.qubit].pulseParams['amp'] = args.amplitude
 
 channelLib.write_to_file()
-print "there"
