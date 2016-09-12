1b8059ef3c2f044e15b9948e5a113fdfce49c657
CoherenceController <qlab@bbn.com>
2016-09-12 08:49:09 -0400

    Calibrate pule amplitude instead of channel
    --DR

--- experiments/muWaveDetection/CRCalSequence.m
+++ experiments/muWaveDetection/CRCalSequence.m
@@ -1,7 +1,11 @@
-function [filename, segmentPoints] = CRCalSequence(control, target, caltype, length)
-
+function [filename, segmentPoints] = CRCalSequence(control, target, caltype, length, varargin)
+if ~isempty(varargin)
+    amplitude = varargin{1};
+else
+    amplitude = 0.8;
+end
 [thisPath, ~] = fileparts(mfilename('fullpath'));
 scriptName = fullfile(thisPath, 'CRCal.py');
-[status, result] = system(sprintf('python "%s" "%s" %s %s %d %d', scriptName, getpref('qlab', 'PyQLabDir'), control, target, caltype, length), '-echo');
+[status, result] = system(sprintf('python "%s" "%s" %s %s %f %f %f', scriptName, getpref('qlab', 'PyQLabDir'), control, target, caltype, length, amplitude), '-echo');
 
 end
