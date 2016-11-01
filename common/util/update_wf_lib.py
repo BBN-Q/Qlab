dc0f76c676ec142fa6381fa4d6ac45e6f6127edf
CoherenceController <qlab@bbn.com>
2016-11-01 09:05:23 -0400

    Choose the pulses to update depending on library

--- common/util/update_wf_lib.py
+++ common/util/update_wf_lib.py
@@ -10,13 +10,17 @@ args = parser.parse_args()
 
 from QGL import *
 from QGL.drivers import APS2Pattern
+from QGL import config
 
 qubits = ChannelLibrary.channelLib.connectivityG.nodes()
 edges = ChannelLibrary.channelLib.connectivityG.edges()
 
 pulseList = []
 for q in qubits:
-	pulseList.append([AC(q, ct) for ct in range(24)])
+	if config.pulse_primitives_lib == 'standard':
+		pulseList.append([AC(q, ct) for ct in range(24)])
+	else:
+		pulseList.append([X90(q), Y90(q), X90m(q), Y90m(q)])
 for edge in edges:
 	pulseList.append(ZX90_CR(edge[0],edge[1]))
 #update waveforms in the desired sequence (generated with APS2Pattern.SAVE_WF_OFFSETS = True)
