ae6892bd600d4bd99dae3f0b617880825fcfa728
gribeill <guilhem.ribeill@gmail.com>
2017-06-08 11:51:52 -0400

    Added note to adjust the python path if necessary. -MW

--- common/util/update_wf_lib.m
+++ common/util/update_wf_lib.m
@@ -1,5 +1,8 @@
 function update_wf_lib(seqPath, seqName)
 
+% note you may need to adjust the path to python depending on your system
+% setup, python version, Anaconda, etc...
+
 [thisPath, ~] = fileparts(mfilename('fullpath'));
 scriptName = fullfile(thisPath, 'update_wf_lib.py');
 [status, result] = system(sprintf('python "%s" "%s" %s %s', scriptName, getpref('qlab', 'PyQLabDir'), seqPath, seqName), '-echo');
